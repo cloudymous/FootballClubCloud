8f4db3f7668a6cbf18549b8a11f47d5cc2080aef
cloudymous <sayacloud@gmail.com>
2018-10-19 16:15:11 +0800

    Initial commit.

---
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/MainMenu.kt
@@ -0,0 +1,11 @@
+package com.example.cloudymous.footballclubcloud
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainMenu : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
