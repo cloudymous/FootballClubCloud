64f3a03a3750bae94f5bc301add7e1a0ec4bd06e
cloudymous <sayacloud@gmail.com>
2018-10-19 23:17:52 +0800

    Lelah.....

--- app/src/main/java/com/example/cloudymous/footballclubcloud/MainMenu.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/MainMenu.kt
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import org.jetbrains.anko.*
+import org.jetbrains.anko.design.snackbar
+import org.jetbrains.anko.generated.design.R.attr.colorAccent
 import org.jetbrains.anko.sdk25.coroutines.onClick
 
 class MainMenu : AppCompatActivity() {
@@ -19,25 +21,57 @@ class MainMenu : AppCompatActivity() {
             verticalLayout {
                 padding = dip(16)
 
-                val name = editText{
+                val name = editText {
                     hint = "What's your name?"
                 }
 
                 linearLayout {
 
-                    button("Say Hello"){
+                    button("Say Hello") {
                         textColor = Color.WHITE
                         onClick { toast("Hello, ${name.text}!") }
                     }.lparams(weight = 1.0F)
 
-                    button ("Say Goodbye"){
+                    button("Say Goodbye") {
                         textColor = Color.WHITE
                         onClick { toast("Goodbye, ${name.text}!") }
                     }.lparams(weight = 1.0F)
 
+                }
+
+                button("Next Intent") {
+                    textColor = Color.WHITE
+                    onClick {
+                        startActivity<DetailClub>()
+                    }
+                }
+
+                button("Say Hello") {
+                    textColor = Color.WHITE
+                    onClick {
+                        alert("Happy Coding!", "Hello, ${name.text}!") {
+                            yesButton { toast("Oh…") }
+                            noButton {}
+                        }.show()
+                    }
+                }.lparams(width = matchParent) {
+                    topMargin = dip(5)
+                }
+
+
+                button("Show Selector") {
+                    textColor = Color.WHITE
+                    onClick {
+                        val club = listOf("Barcelona", "Real Madrid", "Bayern Munchen", "Liverpool")
+                        selector("Hello, ${name.text}! What's football club do you love?", club) { _, i ->
+                            toast("So you love ${club[i]}, right?")
+                        }
+                    }
+                }.lparams(width = matchParent) {
+                    topMargin = dip(5)
+                }
             }
         }
-    }
 
     }
 }
