71f896cf193d1b2f6f92492dd0347445d277c3b5
cloudymous <sayacloud@gmail.com>
2018-10-21 13:39:29 +0800

    Perbaiki arraylist. Buat listener onclik di listview. Masih belum buat adapternya.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/MainMenu.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/MainMenu.kt
@@ -4,9 +4,11 @@ import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
+import android.widget.Toast
 import org.jetbrains.anko.*
 import org.jetbrains.anko.design.snackbar
 import org.jetbrains.anko.generated.design.R.attr.colorAccent
+import org.jetbrains.anko.generated.design.R.attr.layout_anchorGravity
 import org.jetbrains.anko.sdk25.coroutines.onClick
 import java.util.ArrayList
 
@@ -23,21 +25,63 @@ class MainMenu : AppCompatActivity() {
 
     class MainActivityUI : AnkoComponent<MainMenu> {
         override fun createView(ui: AnkoContext<MainMenu>) = with(ui) {
-            verticalLayout {
-                padding = dip (16)
+            linearLayout {
+                padding = dip(16)
+                onClick{toast("Hellow Motto")}
 
+                imageView {
+                    imageResource = R.drawable.img_barca
+
+                }.lparams(dip(50), dip(50))
+
+                textView {
+                    text = "Barcelona"
+                }.lparams {
+                    width = wrapContent
+                    height = wrapContent
+                    margin = dip(10)
+                }
 
             }
         }
 
     }
 
-    private fun initlist(){
-        data class ClubFootball (val nama: String, val imageClub: Int, val descriptionClub: String)
+//    class ListUI : AnkoComponent<MainMenu>{
+//        override fun createView(ui:AnkoContext<MainMenu>) = with(ui){
+//            linearLayout {
+//                padding = dip(16)
+//
+//                imageView {
+//                    imageResource = R.drawable.img_barca
+//
+//                }.lparams(dip(50), dip(50))
+//
+//                textView {
+//                    text = "Barcelona"
+//                }.lparams {
+//                    width = wrapContent
+//                    height = wrapContent
+//                    margin = dip(10)
+//                }
+//
+//            }
+//        }
+//    }
 
-        val clubs: ArrayList<ClubFootball> = ArrayList ()
-        clubs.add(ClubFootball("Real Madrid", R.drawable.ic_launcher_background, "Tim Kesayangan"))
+    private fun initlist() {
 
+        data class ClubFootball(val namaClubs: String, val descriptionClubs: String, val imageClub: Int)
+
+        val clubs: ArrayList<ClubFootball> = ArrayList()
+        clubs.add(ClubFootball("AC Milan", "Tim Kesayangan", R.drawable.img_acm))
+        clubs.add(ClubFootball("Arsenal", "Tim Kesayangan", R.drawable.img_arsenal))
+        clubs.add(ClubFootball("Barcelona", "Tim Kesayangan", R.drawable.img_madrid))
+        clubs.add(ClubFootball("Bayern Munchen", "Tim Kesayangan", R.drawable.img_bayern))
+        clubs.add(ClubFootball("Chelsea", "Tim Kesayangan", R.drawable.img_chelsea))
+        clubs.add(ClubFootball("Manchester City", "Tim Kesayangan", R.drawable.img_city))
+        clubs.add(ClubFootball("Manchester United", "Tim Kesayangan", R.drawable.img_mu))
+        clubs.add(ClubFootball("Real Madrid", "Tim Kesayangan", R.drawable.img_madrid))
     }
 
     //            verticalLayout {
