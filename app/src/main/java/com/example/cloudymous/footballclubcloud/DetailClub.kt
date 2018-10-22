f8603541fbe15dff9e2725b48f68556804fef7d0
cloudymous <sayacloud@gmail.com>
2018-10-22 16:59:04 +0800

    Coba putExtra dan getExtra

--- app/src/main/java/com/example/cloudymous/footballclubcloud/DetailClub.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/DetailClub.kt
@@ -1,34 +1,46 @@
 package com.example.cloudymous.footballclubcloud
 
+import android.content.Intent
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.webkit.WebSettings
+import android.widget.TextView
 import org.jetbrains.anko.*
 
 class DetailClub : AppCompatActivity() {
 
+    private var namaClubExtra: String = ""
+    private var deskripsiClubExtra: String = ""
+    private lateinit var namaClubTV: TextView
+    private lateinit var deskripsiClubTV: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        DetailClubUI().setContentView(this)
-    }
 
-    class DetailClubUI : AnkoComponent<DetailClub> {
-        override fun createView(ui: AnkoContext<DetailClub>) = with(ui) {
-            verticalLayout {
-                padding = dip(16)
-                textView("Nama Club"){
-                    textSize = 20f
-                    textColor = Color.BLACK
-                    textAlignment = View.TEXT_ALIGNMENT_CENTER
-                }
+        verticalLayout {
+            padding = dip(16)
 
-                textView("Club Impian ada di tengah peluh"){
-                    textColor = Color.BLACK
-                }
+            namaClubTV = textView() {
+                textSize = 20f
+                textColor = Color.BLACK
+                textAlignment = View.TEXT_ALIGNMENT_CENTER
+            }
+
+
+            deskripsiClubTV = textView("Club Impian ada di tengah peluh") {
+                textColor = Color.BLACK
             }
         }
 
+        val intent = intent
+        namaClubExtra = intent.getStringExtra("namaClubExt")
+        deskripsiClubExtra = intent.getStringExtra("deskripsiClub")
+
+        namaClubTV.text = namaClubExtra
+        deskripsiClubTV.text = deskripsiClubExtra
+
     }
+
 }
+
--- app/src/main/java/com/example/cloudymous/footballclubcloud/MainMenu.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/MainMenu.kt
@@ -3,14 +3,34 @@ package com.example.cloudymous.footballclubcloud
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk25.coroutines.onClick
 
 class MainMenu : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        MainMenuUI().setContentView(this)
+
     }
 
+    class MainMenuUI : AnkoComponent<MainMenu> {
+        override fun createView(ui: AnkoContext<MainMenu>) = with(ui) {
+            linearLayout {
+                padding = dip(16)
 
+                imageView {
+                    setImageResource(R.drawable.img_madrid)
+                }.lparams(width = dip(50), height = dip(50))
+
+                val namaClub = textView("Real Madrid"){
+                }
+
+                onClick { startActivity<DetailClub>("namaClubExt" to "${namaClub.text}", "deskripsiClub" to "Impian Ada di tengah peluh" ) }
+
+            }
+        }
+    }
 
 
 
