92f3856b4d937bf9728bd4037bf4068856a1f88f
cloudymous <sayacloud@gmail.com>
2018-10-22 11:14:21 +0800

    Bersihkan semua ke-erroran ini. Anko bikin pusing :(

--- app/src/main/java/com/example/cloudymous/footballclubcloud/MainMenu.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/MainMenu.kt
@@ -3,6 +3,7 @@ package com.example.cloudymous.footballclubcloud
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.AnkoContext
 
 class MainMenu : AppCompatActivity() {
 
