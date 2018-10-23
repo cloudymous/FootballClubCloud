24ad4144736c189176bdd3a55248869ed5776ac9
cloudymous <sayacloud@gmail.com>
2018-10-23 14:09:58 +0800

    Ubah tampilan. Diperbaiki

--- app/src/main/java/com/example/cloudymous/footballclubcloud/MainMenu.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/MainMenu.kt
@@ -3,6 +3,7 @@ package com.example.cloudymous.footballclubcloud
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
 import android.view.Gravity
 import org.jetbrains.anko.*
 import org.jetbrains.anko.sdk25.coroutines.onClick
@@ -18,6 +19,7 @@ class MainMenu : AppCompatActivity() {
     class MainMenuUI : AnkoComponent<MainMenu> {
         override fun createView(ui: AnkoContext<MainMenu>) = with(ui) {
             linearLayout {
+                lparams(width = matchParent, height = wrapContent)
                 padding = dip(16)
 
                 imageView {
@@ -25,7 +27,9 @@ class MainMenu : AppCompatActivity() {
                 }.lparams(width = dip(50), height = dip(50))
 
                 val namaClub = textView("Real Madrid"){
+                }.lparams{
                     gravity = Gravity.CENTER_VERTICAL
+                    marginStart = dip(16)
                 }
 
                 onClick { startActivity<DetailClub>("namaClubExt" to "${namaClub.text}", "deskripsiClub" to "Impian Ada di tengah peluh" ) }
