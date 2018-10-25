2a0e7adb3cf4eadab86c2e9c5a21f44da23a6b30
cloudymous <sayacloud@gmail.com>
2018-10-25 18:38:36 +0800

    Edit adapter

--- app/src/main/java/com/example/cloudymous/footballclubcloud/ClubAdapter.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/ClubAdapter.kt
@@ -3,30 +3,32 @@ package com.example.cloudymous.footballclubcloud
 import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.*
+import android.widget.ImageView
+import android.widget.TextView
 import org.jetbrains.anko.AnkoContext
 
 
-class ClubAdapter (private val context: Context, private val items: List<Clubs>, private val listener: (Clubs) -> Unit)
-    : RecyclerView.Adapter<ClubAdapter.ViewHolder>(){
+class RecyclerViewAdapter(private val context: Context, private val items: List<Clubs>)
+    : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-//        ViewHolder(LayoutInflater.from(context).inflate(ListClubUI(), parent, false))
-        ViewHolder{
-            return ViewHolder (ListClubUI().createView(AnkoContext.create(parent!!.context)))
-        }
+        ViewHolder(LayoutInflater.from(context).inflate(ListClubUI, parent, false))
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bindItem(items[position], listener )
+        holder.bindItem(items[position])
     }
 
     override fun getItemCount(): Int = items.size
 
-    class ViewHolder (view: View) : RecyclerView.ViewHolder(view){
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
 
-        fun bindItem(items: Clubs, listener: (Clubs) -> Unit) {
+        private val name = view.findViewById<TextView>(R.id.name)
+        private val deskripsi = view.findViewById<TextView>(R.id.desk)
+        private val gambar = view.findViewById<ImageView>(R.id.gambar)
 
+        fun bindItem(items: Clubs) {
+            name.text = items.name
+            items.?.let { Picasso.get().load(it).into(gambar) }
         }
-
     }
-
 }
