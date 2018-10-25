9204c7e509f46bb2c8f60f0f771fd823dbfa29bb
cloudymous <sayacloud@gmail.com>
2018-10-25 17:36:28 +0800

    Edit Adapter.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/ClubAdapter.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/ClubAdapter.kt
@@ -3,13 +3,17 @@ package com.example.cloudymous.footballclubcloud
 import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.*
+import org.jetbrains.anko.AnkoContext
 
 
 class ClubAdapter (private val context: Context, private val items: List<Clubs>, private val listener: (Clubs) -> Unit)
     : RecyclerView.Adapter<ClubAdapter.ViewHolder>(){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-        ViewHolder(LayoutInflater.from(context).inflate(ListClubUI(), parent, false))
+//        ViewHolder(LayoutInflater.from(context).inflate(ListClubUI(), parent, false))
+        ViewHolder{
+            return ViewHolder (ListClubUI().createView(AnkoContext.create(parent!!.context)))
+        }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bindItem(items[position], listener )
