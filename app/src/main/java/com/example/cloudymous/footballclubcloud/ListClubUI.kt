827829be23ba7ee7751ab5384f29bcd36a5faeb8
cloudymous <sayacloud@gmail.com>
2018-10-25 17:35:56 +0800

    Buat val id untuk id

--- app/src/main/java/com/example/cloudymous/footballclubcloud/ListClubUI.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/ListClubUI.kt
@@ -7,21 +7,21 @@ import org.jetbrains.anko.*
 
 class ListClubUI : AnkoComponent<MainMenu> {
 
-//    val IdImageView = 1
-//    val IdClubName = 2
+    val IdImageView = 1
+    val IdClubName = 2
 
     override fun createView(ui: AnkoContext<MainMenu>) = with(ui) {
-        linearLayout {
+        var list_club = linearLayout {
             lparams(width = matchParent, height = wrapContent)
             padding = dip(16)
 
             imageView {
-                id = "IdImageView"
+                id = IdImageView
                 setImageResource(R.drawable.img_madrid)
             }.lparams(width = dip(50), height = dip(50))
 
             textView("Real Madrid"){
-                id = "IdClubName"
+                id = IdClubName
                 textSize = 18f
                 textColor = Color.BLACK
                 textAlignment = View.TEXT_ALIGNMENT_CENTER
