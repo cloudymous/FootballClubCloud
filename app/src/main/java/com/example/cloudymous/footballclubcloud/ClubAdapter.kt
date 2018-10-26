3a4f02c40881156b6842e5bf13d8ba58d658f7d9
cloudymous <sayacloud@gmail.com>
2018-10-26 09:41:03 +0800

    Buat listener untuk handle onClick

--- app/src/main/java/com/example/cloudymous/footballclubcloud/ClubAdapter.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/ClubAdapter.kt
@@ -7,9 +7,10 @@ import android.view.*
 import android.widget.ImageView
 import android.widget.TextView
 import com.squareup.picasso.Picasso
+import kotlinx.android.extensions.LayoutContainer
 import org.jetbrains.anko.*
 
-class ClubAdapter(private val context: Context, private val items: List<Clubs>)
+class ClubAdapter(private val context: Context, private val items: List<Clubs>, private val listener: (Clubs) -> Unit)
     : RecyclerView.Adapter<ClubAdapter.TeamViewHolder>() {
 
 //    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
@@ -20,21 +21,28 @@ class ClubAdapter(private val context: Context, private val items: List<Clubs>)
     }
 
     override fun onBindViewHolder(holder: TeamViewHolder, position: Int) {
-        holder.bindItem(items[position])
+        holder.bindItem(items[position], listener)
     }
 
     override fun getItemCount(): Int = items.size
 
     class TeamViewHolder(view: View) : RecyclerView.ViewHolder(view){
 
+//    class TeamViewHolder (override val containerView: View) : RecyclerView.ViewHolder(containerView),
+//        LayoutContainer {
         private val name = view.findViewById<TextView>(R.id.nama_club)
-//        private val deskripsi = view.findViewById<TextView>(R.id.desk_club)
+////        private val deskripsi = view.findViewById<TextView>(R.id.desk_club)
         private val gambar = view.findViewById<ImageView>(R.id.gambar_club)
 
-        fun bindItem(items: Clubs) {
+        fun bindItem(items: Clubs, listener: (Clubs) -> Unit) {
+
             name.text = items.namaClub
 //            deskripsi.text = items.deskripsiClub
             items.imageClub?.let { Picasso.get().load(it).into(gambar) }
+            itemView.setOnClickListener{
+                listener(items)
+            }
+
         }
     }
 }
