6d682afdf05f2738d9102b7bf7d75f3a427a71fc
cloudymous <sayacloud@gmail.com>
2018-10-29 11:39:43 +0800

    Create adapter and spinner adapter onSelectedListener

--- app/src/main/java/com/example/cloudymous/footballclubcloud/View/MainActivity.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/View/MainActivity.kt
@@ -6,10 +6,14 @@ import android.os.Bundle
 import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.widget.LinearLayout
-import android.widget.ProgressBar
-import android.widget.Spinner
+import android.view.View
+import android.widget.*
+import com.example.cloudymous.footballclubcloud.Api.ApiRepository
+import com.example.cloudymous.footballclubcloud.Model.Team
+import com.example.cloudymous.footballclubcloud.Presenter.MainAdapter
+import com.example.cloudymous.footballclubcloud.Presenter.MainPresenter
 import com.example.cloudymous.footballclubcloud.R.color.colorAccent
+import com.google.gson.Gson
 
 import org.jetbrains.anko.*
 import org.jetbrains.anko.recyclerview.v7.recyclerView
@@ -17,13 +21,41 @@ import org.jetbrains.anko.support.v4.swipeRefreshLayout
 
 class MainActivity : AppCompatActivity() {
 
+    private var teams: MutableList<Team> = mutableListOf()
+    private lateinit var presenter: MainPresenter
+    private lateinit var adapter: MainAdapter
+
     private lateinit var listTeam: RecyclerView
     private lateinit var progressBar: ProgressBar
     private lateinit var swipeRefresh: SwipeRefreshLayout
     private lateinit var spinner: Spinner
+    private lateinit var leagueName: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        adapter = MainAdapter(Team)
+        listTeam.adapter = adapter
+
+        val request = ApiRepository()
+        val gson = Gson()
+
+        presenter = MainPresenter(this, request, gson)
+
+        val spinnerItems = resources.getStringArray(league)
+        val spinnerAdapter = ArrayAdapter(ctx, android.R.layout.simple_spinner_dropdown_item, spinnerItems)
+        spinner.adapter = spinnerAdapter
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                leagueName = spinner.selectedItem.toString()
+                presenter.getTeamList(leagueName)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+        }
+
 
         linearLayout {
             lparams(width = matchParent, height = wrapContent)
