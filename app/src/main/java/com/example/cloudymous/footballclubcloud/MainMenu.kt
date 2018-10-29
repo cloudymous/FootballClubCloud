7bfedb9ca9ea6ea763a0b33b9af7dbb27fc31a75
cloudymous <sayacloud@gmail.com>
2018-10-29 10:34:03 +0800

    Create new layout

--- app/src/main/java/com/example/cloudymous/footballclubcloud/MainMenu.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/MainMenu.kt
@@ -3,60 +3,60 @@ package com.example.cloudymous.footballclubcloud
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import com.example.cloudymous.footballclubcloud.Club.ClubAdapter
-import com.example.cloudymous.footballclubcloud.Club.Clubs
-import com.example.cloudymous.footballclubcloud.R.array.*
-import com.example.cloudymous.footballclubcloud.View.DetailClub
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import android.widget.Spinner
+import com.example.cloudymous.footballclubcloud.R.color.colorAccent
+
 import org.jetbrains.anko.*
 import org.jetbrains.anko.recyclerview.v7.recyclerView
+import org.jetbrains.anko.support.v4.swipeRefreshLayout
 
 class MainMenu : AppCompatActivity() {
 
-    private val clubs: MutableList<Clubs> = mutableListOf()
+    private lateinit var listTeam: RecyclerView
+    private lateinit var progressBar: ProgressBar
+    private lateinit var swipeRefresh: SwipeRefreshLayout
+    private lateinit var spinner: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        MainMenuUI().setContentView(this)
 
-        val list = findViewById<RecyclerView>(R.id.listnya)
+        linearLayout {
+            lparams(width = matchParent, height = wrapContent)
+            orientation = LinearLayout.VERTICAL
+            topPadding = dip(16)
+            leftPadding = dip(16)
+            rightPadding = dip(16)
 
-        loadData()
+            spinner = spinner()
+            swipeRefresh = swipeRefreshLayout {
+                setColorSchemeResources(
+                    colorAccent,
+                    android.R.color.holo_green_light,
+                    android.R.color.holo_orange_light,
+                    android.R.color.holo_red_light
+                )
 
-        list.layoutManager = LinearLayoutManager(this)
-        list.adapter = ClubAdapter(this, clubs) {
-            startActivity<DetailClub>("ClubExtra" to it)
-        }
-
-    }
-
-    class MainMenuUI : AnkoComponent<MainMenu> {
-
-        override fun createView(ui: AnkoContext<MainMenu>) = with(ui) {
-            linearLayout {
-                lparams(width = matchParent, height = wrapContent)
-
-                recyclerView {
-                    id = R.id.listnya
+                relativeLayout {
                     lparams(width = matchParent, height = wrapContent)
+
+                    listTeam = recyclerView {
+                        lparams(width = matchParent, height = wrapContent)
+                        layoutManager = LinearLayoutManager(ctx)
+                    }
+
+                    progressBar = progressBar {
+                    }.lparams {
+                        centerHorizontally()
+                    }
                 }
             }
         }
-    }
 
-    private fun loadData() {
-        val nama = resources.getStringArray(club_name)
-        val desc = resources.getStringArray(club_desc)
-        val image = resources.obtainTypedArray(club_image)
-
-        clubs.clear()
-
-        for (i in nama.indices) {
-            clubs.add(Clubs(nama[i], desc[i], image.getResourceId(i, 0)))
-        }
-
-        image.recycle()
     }
 
 
