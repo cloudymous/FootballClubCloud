3347ded6ebb58d22dd0e29d1ac39e2e39e6806d1
cloudymous <sayacloud@gmail.com>
2018-10-30 13:13:43 +0800

    Change name to MatchPagerAdapter

--- app/src/main/java/com/example/cloudymous/footballclubcloud/Presenter/MatchPagerAdapter.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/Presenter/MatchPagerAdapter.kt
@@ -6,7 +6,7 @@ import android.support.v4.app.FragmentPagerAdapter
 import com.example.cloudymous.footballclubcloud.View.LastMatchFragment
 import com.example.cloudymous.footballclubcloud.View.NextMatchFragment
 
-class PagerAdapter (fm: FragmentManager) : FragmentPagerAdapter(fm) {
+class MatchPagerAdapter (fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
     private val pages = listOf(
         LastMatchFragment(),
