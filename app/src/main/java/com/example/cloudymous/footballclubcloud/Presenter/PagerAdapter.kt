b61ec4e9b137f551fa4d0781252e6162da2dc6ae
cloudymous <sayacloud@gmail.com>
2018-10-30 13:11:10 +0800

    Create PagerAdapter for handling tabs

---
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/Presenter/PagerAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.cloudymous.footballclubcloud.Presenter
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import com.example.cloudymous.footballclubcloud.View.LastMatchFragment
+import com.example.cloudymous.footballclubcloud.View.NextMatchFragment
+
+class PagerAdapter (fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    private val pages = listOf(
+        LastMatchFragment(),
+        NextMatchFragment()
+    )
+
+    override fun getItem(position: Int): Fragment {
+        return pages[position]
+    }
+
+    override fun getCount(): Int {
+        return pages.size
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return if (position == 0){
+            "Last Match"
+        } else {
+            "Next Match"
+        }
+    }
+}
