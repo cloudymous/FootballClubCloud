ace7777a21859a9e3691c6d25d86ed3f61e85314
cloudymous <sayacloud@gmail.com>
2018-10-30 13:14:18 +0800

    Change Main Activity layout.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/View/MainActivity.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/View/MainActivity.kt
@@ -3,115 +3,19 @@ package com.example.cloudymous.footballclubcloud.View
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.widget.SwipeRefreshLayout
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.view.View
-import android.widget.*
-import com.example.cloudymous.footballclubcloud.Api.ApiRepository
-import com.example.cloudymous.footballclubcloud.Model.Team
-import com.example.cloudymous.footballclubcloud.Presenter.MainAdapter
-import com.example.cloudymous.footballclubcloud.Presenter.MainPresenter
+import com.example.cloudymous.footballclubcloud.Presenter.MatchPagerAdapter
 import com.example.cloudymous.footballclubcloud.R
-import com.example.cloudymous.footballclubcloud.R.color.colorAccent
-import com.example.cloudymous.footballclubcloud.Utils.invisible
-import com.example.cloudymous.footballclubcloud.Utils.visible
-import com.google.gson.Gson
+import kotlinx.android.synthetic.main.main_activity.*
 
-import org.jetbrains.anko.*
-import org.jetbrains.anko.recyclerview.v7.recyclerView
-import org.jetbrains.anko.support.v4.onRefresh
-import org.jetbrains.anko.support.v4.swipeRefreshLayout
-
-class MainActivity : AppCompatActivity(), MainView {
-
-    private var teams: MutableList<Team> = mutableListOf()
-    private lateinit var presenter: MainPresenter
-    private lateinit var adapter: MainAdapter
-
-    private lateinit var listTeam: RecyclerView
-    private lateinit var progressBar: ProgressBar
-    private lateinit var swipeRefresh: SwipeRefreshLayout
-    private lateinit var spinner: Spinner
-    private lateinit var leagueName: String
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
 
+        viewpager_main.adapter = MatchPagerAdapter(supportFragmentManager)
+        tabs_main.setupWithViewPager(viewpager_main)
 
-        linearLayout {
-            lparams(width = matchParent, height = wrapContent)
-            orientation = LinearLayout.VERTICAL
-            topPadding = dip(16)
-            leftPadding = dip(16)
-            rightPadding = dip(16)
-
-            spinner = spinner()
-            swipeRefresh = swipeRefreshLayout {
-                setColorSchemeResources(
-                    colorAccent,
-                    android.R.color.holo_green_light,
-                    android.R.color.holo_orange_light,
-                    android.R.color.holo_red_light
-                )
-
-                relativeLayout {
-                    lparams(width = matchParent, height = wrapContent)
-
-                    listTeam = recyclerView {
-                        lparams(width = matchParent, height = wrapContent)
-                        layoutManager = LinearLayoutManager(ctx)
-                    }
-
-                    progressBar = progressBar {
-                    }.lparams {
-                        centerHorizontally()
-                    }
-                }
-            }
-        }
-
-        val spinnerItems = resources.getStringArray(R.array.league)
-        val spinnerAdapter = ArrayAdapter(ctx, android.R.layout.simple_spinner_dropdown_item, spinnerItems)
-        spinner.adapter = spinnerAdapter
-
-        adapter = MainAdapter(teams)
-        listTeam.adapter = adapter
-
-        val request = ApiRepository()
-        val gson = Gson()
-
-        presenter = MainPresenter(this, request, gson)
-
-        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
-                leagueName = spinner.selectedItem.toString()
-                presenter.getTeamList(leagueName)
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {}
-        }
-
-        swipeRefresh.onRefresh {
-            presenter.getTeamList(leagueName)
-        }
 
     }
-
-    override fun showLoading(){
-        progressBar.visible()
-    }
-
-    override fun hideLoading() {
-        progressBar.invisible()
-    }
-
-    override fun showTeamList(data: List<Team>) {
-       swipeRefresh.isRefreshing = false
-        teams.clear()
-        teams.addAll(data)
-        adapter.notifyDataSetChanged()
-    }
-
-
 }
