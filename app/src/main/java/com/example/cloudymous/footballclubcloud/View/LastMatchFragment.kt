a94074ca42476c0d3e8fccb3a36f38a695f09282
cloudymous <sayacloud@gmail.com>
2018-10-30 14:18:01 +0800

    Cleaning the code

--- app/src/main/java/com/example/cloudymous/footballclubcloud/View/LastMatchFragment.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/View/LastMatchFragment.kt
@@ -8,23 +8,12 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.cloudymous.footballclubcloud.R
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
 class LastMatchFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_last_match, container, false)
     }
 
