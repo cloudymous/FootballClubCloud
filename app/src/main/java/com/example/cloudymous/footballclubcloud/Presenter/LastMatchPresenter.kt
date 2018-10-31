9b206b6070346b3691a87266ac7ce7a774b79781
cloudymous <sayacloud@gmail.com>
2018-10-31 14:01:21 +0800

    Create Last Match Presenter

---
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/Presenter/LastMatchPresenter.kt
@@ -0,0 +1,29 @@
+package com.example.cloudymous.footballclubcloud.Presenter
+
+import com.example.cloudymous.footballclubcloud.Api.ApiRepository
+import com.example.cloudymous.footballclubcloud.Api.TheSportDBApi
+import com.example.cloudymous.footballclubcloud.Model.LastMatchResponse
+import com.example.cloudymous.footballclubcloud.View.LastMatchView
+import com.google.gson.Gson
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+
+class LastMatchPresenter (private val view: LastMatchView,
+                          private val apiRepository: ApiRepository,
+                          private val gson: Gson) {
+
+    fun getLastMatch(leagueId: String?){
+        view.showLoading()
+        doAsync {
+            val data = gson.fromJson(apiRepository
+                .doRequest(TheSportDBApi.getLastMatch(leagueId)),
+                LastMatchResponse::class.java
+            )
+
+            uiThread {
+                view.hideLoading()
+                view.showLastMatchList(data.LastMatch)
+            }
+        }
+    }
+}
