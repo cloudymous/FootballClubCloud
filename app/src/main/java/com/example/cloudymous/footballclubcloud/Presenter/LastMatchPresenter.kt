c5aad349e833543aac7ea8b1a7eee94746b2f646
cloudymous <sayacloud@gmail.com>
2018-11-01 16:24:57 +0800

    Rename the data class variable

--- app/src/main/java/com/example/cloudymous/footballclubcloud/Presenter/LastMatchPresenter.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/Presenter/LastMatchPresenter.kt
@@ -22,7 +22,7 @@ class LastMatchPresenter (private val view: LastMatchView,
 
             uiThread {
                 view.hideLoading()
-                view.showLastMatchList(data.LastMatch)
+                view.showLastMatchList(data.lastmatch)
             }
         }
     }
