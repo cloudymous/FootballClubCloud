0d242a89c0e7d1dad77cf4f93ca42b5274f2f2e1
padivalley <it.kalaborang@gmail.com>
2018-11-02 16:25:30 +0800

    Change some line of code remove last match class

--- app/src/main/java/com/example/cloudymous/footballclubcloud/View/MainView.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/View/MainView.kt
@@ -1,5 +1,6 @@
 package com.example.cloudymous.footballclubcloud.View
 
+import com.example.cloudymous.footballclubcloud.Model.LastMatch
 import com.example.cloudymous.footballclubcloud.Model.Team
 
 interface MainView {
