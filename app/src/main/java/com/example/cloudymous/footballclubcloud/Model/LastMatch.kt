fe45c71238061acffa72c952212f871459d0837f
padivalley <it.kalaborang@gmail.com>
2018-11-02 16:24:46 +0800

    Repair class structure

--- app/src/main/java/com/example/cloudymous/footballclubcloud/Model/LastMatch.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/Model/LastMatch.kt
@@ -4,9 +4,6 @@ import com.google.gson.annotations.SerializedName
 
 
 data class LastMatch (
-    @SerializedName("idEvent")
-    var eventId : String? = null,
-
     @SerializedName("strEvent")
     var eventName : String? = null
 )
