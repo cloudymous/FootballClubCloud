ce8f5957da5e4e87d6386c4512a24bd325a49163
cloudymous <sayacloud@gmail.com>
2018-11-05 09:39:59 +0800

    Add getNextMatch function for get the last match from API.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/Api/TheSportDBApi.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/Api/TheSportDBApi.kt
@@ -30,4 +30,18 @@ object TheSportDBApi {
 
 
     }
+
+    fun getNextMatch(leagueId: String?) : String {
+        return Uri.parse(BuildConfig.BASE_URL).buildUpon()
+            .appendPath("api")
+            .appendPath("v1")
+            .appendPath("json")
+            .appendPath(BuildConfig.TSDB_API_KEY)
+            .appendPath("eventsnextleague.php")
+            .appendQueryParameter("id", leagueId)
+            .build()
+            .toString()
+
+
+    }
 }
