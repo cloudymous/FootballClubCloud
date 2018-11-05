8f0cca294d8e1bdc86489d9b0c10f95c33d1afe1
cloudymous <sayacloud@gmail.com>
2018-11-05 14:20:42 +0800

    Recreate LastMatch data class with new variable.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/Model/LastMatch.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/Model/LastMatch.kt
@@ -4,6 +4,19 @@ import com.google.gson.annotations.SerializedName
 
 
 data class LastMatch (
-    @SerializedName("strEvent")
-    var eventName : String? = null
+    @SerializedName("strDate")
+    var eventDate : String? = null,
+
+    @SerializedName("strHomeTeam")
+    var homeTeam: String? = null,
+
+    @SerializedName("strAwayTeam")
+    var awayTeam: String? = null,
+
+    @SerializedName("intHomeScore")
+    var homeScore: String? = null,
+
+    @SerializedName("intAwayScore")
+    var awayScore: String? = null
+
 )
