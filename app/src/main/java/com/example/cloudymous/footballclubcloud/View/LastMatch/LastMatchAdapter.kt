45c4f588a96230021f58090028c6bbd04220244a
cloudymous <sayacloud@gmail.com>
2018-11-05 14:21:20 +0800

    Remove anko and replace with XML for handling layout.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/View/LastMatch/LastMatchAdapter.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/View/LastMatch/LastMatchAdapter.kt
@@ -1,25 +1,25 @@
 package com.example.cloudymous.footballclubcloud.View.LastMatch
 
+import android.content.Context
 import android.graphics.Color
 import android.support.v7.widget.RecyclerView
 import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
 import com.example.cloudymous.footballclubcloud.Model.LastMatch
 import com.example.cloudymous.footballclubcloud.R
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.list_match_experimental.*
 import org.jetbrains.anko.*
 
-class LastMatchAdapter (private val lastmatch : List<LastMatch>) : RecyclerView.Adapter<LastMatchViewHolder>() {
+class LastMatchAdapter (private val context: Context, private val lastmatch : List<LastMatch>) :
+    RecyclerView.Adapter<LastMatchViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, position: Int): LastMatchViewHolder {
-        return LastMatchViewHolder(
-            LastMatchUI().createView(
-                AnkoContext.create(parent.context, parent)
-            )
-        )
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, position: Int): LastMatchViewHolder =
+        LastMatchViewHolder(LayoutInflater.from(context).inflate(R.layout.list_match_experimental, parent, false))
 
     override fun getItemCount(): Int = lastmatch.size
 
@@ -29,37 +29,14 @@ class LastMatchAdapter (private val lastmatch : List<LastMatch>) : RecyclerView.
 
 }
 
-class LastMatchViewHolder (view: View) : RecyclerView.ViewHolder(view){
-
-    private val lastEventName: TextView = view.find(R.id.event_name)
+class LastMatchViewHolder (override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer {
 
     fun bindItem(lastmatch: LastMatch) {
-        lastEventName.text = lastmatch.eventName
+        home.text = lastmatch.homeTeam
+        home_score.text = lastmatch.homeScore
+        away.text = lastmatch.awayTeam
+        away_score = lastmatch.awayScore
+        time.text = lastmatch.eventDate
     }
 
-}
-
-class LastMatchUI : AnkoComponent<ViewGroup> {
-    override fun createView(ui: AnkoContext<ViewGroup>): View {
-        return with(ui) {
-            linearLayout {
-                lparams(width = matchParent, height = wrapContent)
-                padding = dip(8)
-                orientation = LinearLayout.HORIZONTAL
-                gravity = Gravity.CENTER
-
-                textView ("Main Menu") {
-                    id = R.id.event_name
-                    textSize = 16f
-                    textColor = Color.BLACK
-                    textAlignment = View.TEXT_ALIGNMENT_CENTER
-                    gravity = Gravity.CENTER_HORIZONTAL
-                }.lparams {
-                    margin = dip(15)
-                    gravity = Gravity.CENTER
-                }
-
-            }
-        }
-    }
 }
