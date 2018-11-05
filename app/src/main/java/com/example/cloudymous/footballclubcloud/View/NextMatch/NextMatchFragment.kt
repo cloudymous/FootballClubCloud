37ce6e6af48ac9f781b27f229fdd7f4e72c7dfae
cloudymous <sayacloud@gmail.com>
2018-11-05 11:24:51 +0800

    Add fun to load data and present data to fragment.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/View/NextMatch/NextMatchFragment.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/View/NextMatch/NextMatchFragment.kt
@@ -3,13 +3,29 @@ package com.example.cloudymous.footballclubcloud.View.NextMatch
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.cloudymous.footballclubcloud.Api.ApiRepository
+import com.example.cloudymous.footballclubcloud.Model.NextMatch
+import com.example.cloudymous.footballclubcloud.Presenter.NextMatchPresenter
 
 import com.example.cloudymous.footballclubcloud.R
+import com.example.cloudymous.footballclubcloud.Utils.invisible
+import com.example.cloudymous.footballclubcloud.Utils.visible
+import com.example.cloudymous.footballclubcloud.View.NextMatchView
+import com.google.gson.Gson
+import kotlinx.android.synthetic.main.fragment_next_match.*
+import org.jetbrains.anko.support.v4.ctx
+import org.jetbrains.anko.support.v4.onRefresh
 
-class NextMatchFragment : Fragment() {
+class NextMatchFragment : Fragment(), NextMatchView {
+
+    private val nextmatch: MutableList<NextMatch> = mutableListOf()
+
+    private lateinit var adapter: NextMatchAdapter
+    private lateinit var presenter: NextMatchPresenter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +33,41 @@ class NextMatchFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_next_match, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        adapter = NextMatchAdapter(ctx, nextmatch)
+        next_match_list.layoutManager = LinearLayoutManager(context)
+        next_match_list.adapter = adapter
+
+        val request = ApiRepository()
+        val gson = Gson()
+
+        presenter = NextMatchPresenter(this,request, gson)
+        presenter.getNextMatch("4328")
+
+
+        swipe_refresh.onRefresh {
+            presenter.getNextMatch("4328")
+        }
+    }
+
+    override fun showLoading() {
+        progress_bar.visible()
+
+    }
+
+    override fun hideLoading() {
+        progress_bar.invisible()
+    }
+
+    override fun showNextMatchList(data: List<NextMatch>) {
+        swipe_refresh.isRefreshing = false
+        nextmatch.clear()
+        nextmatch.addAll(data)
+        adapter.notifyDataSetChanged()
     }
 
 
