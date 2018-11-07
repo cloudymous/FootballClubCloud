31c0b21a6bde342df52915bcaaf78615d27062c2
cloudymous <sayacloud@gmail.com>
2018-11-07 11:19:01 +0800

    Rename to make it easier to recognize.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/View/MainActivity.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/View/MainActivity.kt
@@ -3,15 +3,14 @@ package com.example.cloudymous.footballclubcloud.View
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.example.cloudymous.footballclubcloud.View.MainPagerAdapter
 import com.example.cloudymous.footballclubcloud.R
-import kotlinx.android.synthetic.main.main_activity.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        setContentView(R.layout.activity_main)
 
         viewpager_main.adapter = MainPagerAdapter(supportFragmentManager)
         tabs_main.setupWithViewPager(viewpager_main)
