cb649b04ec826f8ad8e5a4100cd6245cfe273d5e
cloudymous <sayacloud@gmail.com>
2018-11-08 15:32:54 +0800

    Create layout and some function to home activity.

---
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/View/HomeActivity.kt
@@ -0,0 +1,44 @@
+package com.example.cloudymous.footballclubcloud.View
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cloudymous.footballclubcloud.R
+import com.example.cloudymous.footballclubcloud.R.id.team
+import com.example.cloudymous.footballclubcloud.R.layout.activity_home
+import com.example.cloudymous.footballclubcloud.R.id.favorites
+import kotlinx.android.synthetic.main.activity_home.*
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activity_home)
+
+        bottom_navigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                team -> {
+                    loadTeamFragment(savedInstanceState)
+                }
+                favorites -> {
+                    loadFavoriteFragment(savedInstanceState)
+                }
+            }
+            true
+        }
+        bottom_navigation.selectedItemId = team
+    }
+
+    private fun loadTeamFragment(savedInstanceState: Bundle?) {
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.main_container, TeamFragment(), TeamFragment::class.java.simpleName).commit()
+        }
+    }
+
+    private fun loadFavoriteFragment(savedInstanceState: Bundle?) {
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.main_container, FavoriteTeamFragment(), FavoriteTeamFragment::class.java.simpleName).commit()
+        }
+    }
+}
