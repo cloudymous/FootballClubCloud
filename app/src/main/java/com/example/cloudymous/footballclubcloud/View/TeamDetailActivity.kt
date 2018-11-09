838cbec645ba3ce9d2779c5817d047ba70edbde1
cloudymous <sayacloud@gmail.com>
2018-11-09 11:34:29 +0800

    Create view TeamDetailActivity with Anko.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/View/TeamDetailActivity.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/View/TeamDetailActivity.kt
@@ -1,13 +1,129 @@
 package com.example.cloudymous.footballclubcloud.View
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.example.cloudymous.footballclubcloud.R
+import android.support.design.R.attr.colorAccent
+import android.support.design.R.attr.colorPrimary
+import android.support.v4.content.ContextCompat
+import android.support.v4.widget.SwipeRefreshLayout
+import android.view.Gravity
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import android.widget.TextView
+import com.example.cloudymous.footballclubcloud.Api.ApiRepository
+import com.example.cloudymous.footballclubcloud.Model.Team
+import com.example.cloudymous.footballclubcloud.Presenter.TeamDetailPresenter
+import com.example.cloudymous.footballclubcloud.Utils.invisible
+import com.example.cloudymous.footballclubcloud.Utils.visible
+import com.google.gson.Gson
+import org.jetbrains.anko.*
+import org.jetbrains.anko.support.v4.onRefresh
+import org.jetbrains.anko.support.v4.swipeRefreshLayout
 
-class TeamDetailActivity : AppCompatActivity() {
+class TeamDetailActivity : AppCompatActivity(), TeamDetailView {
+
+    private lateinit var progressBar: ProgressBar
+    private lateinit var swipeRefresh: SwipeRefreshLayout
+    private lateinit var teamBadge: ImageView
+    private lateinit var teamName: TextView
+    private lateinit var teamFormedYear: TextView
+    private lateinit var teamStadium: TextView
+    private lateinit var teamDescription: TextView
+
+    private lateinit var presenter: TeamDetailPresenter
+    private lateinit var teams: Team
+    private lateinit var id: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_team_detail)
+
+        val intent = intent
+        id = intent.getStringExtra("id")
+        val request = ApiRepository()
+        val gson = Gson()
+
+        presenter = TeamDetailPresenter(this, request, gson)
+        presenter.getTeamList(id)
+        swipeRefresh.onRefresh {
+            presenter.getTeamList(id)
+        }
+
+        supportActionBar?.title = "Team Detail"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        linearLayout {
+            lparams(width = matchParent, height = wrapContent)
+            orientation = LinearLayout.VERTICAL
+            backgroundColor = Color.WHITE
+            swipeRefresh = swipeRefreshLayout {
+                setColorSchemeResources(
+                    colorAccent,
+                    android.R.color.holo_green_light,
+                    android.R.color.holo_orange_light,
+                    android.R.color.holo_red_light
+                )
+
+                scrollView {
+                    isVerticalScrollBarEnabled = false
+
+                    relativeLayout {
+                        lparams(width = matchParent, height = wrapContent)
+
+                        linearLayout {
+                            lparams(width = matchParent, height = wrapContent)
+                            padding = dip(10)
+                            orientation = LinearLayout.VERTICAL
+                            gravity = Gravity.CENTER_HORIZONTAL
+
+                            teamBadge = imageView {}.lparams(height = dip(75))
+                            teamName = textView {
+                                this.gravity = Gravity.CENTER
+                                textSize = 20f
+                                textColor = ContextCompat.getColor(context, colorAccent)
+                            }.lparams {
+                                topMargin = dip(5)
+                            }
+                            teamFormedYear = textView {
+                                this.gravity = Gravity.CENTER
+                            }
+                            teamStadium = textView {
+                                this.gravity = Gravity.CENTER
+                                textColor = ContextCompat.getColor(context, colorPrimary)
+                            }
+                            teamDescription = textView().lparams {
+                                topMargin = dip(20)
+                            }
+                        }
+                        progressBar = progressBar {}.lparams {
+                            centerHorizontally()
+                        }
+                    }
+                }
+            }
+        }
     }
+
+    override fun showLoading() {
+        progressBar.visible()
+    }
+
+    override fun hideLoading() {
+        progressBar.invisible()
+    }
+
+    override fun showTeamDetail(data: List<Team>) {
+        teams = Team(
+            data[0].teamId,
+            data[0].teamName,
+            data[0].teamBadge)
+        swipeRefresh.isRefreshing= false
+        teamName.text = data[0].teamName
+        teamDescription.text = data[0].teamDescription
+        teamFormedYear.text = data[0].teamFormatYear
+        teamStadium.text = data[0].teamStadium
+
+    }
+
 }
