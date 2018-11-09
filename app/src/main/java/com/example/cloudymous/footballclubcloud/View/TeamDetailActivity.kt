77f2b799cd534177a9154889360c7c152c6f8bbb
cloudymous <sayacloud@gmail.com>
2018-11-09 15:50:04 +0800

    Add menu and create funtion to add team to SQLite.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/View/TeamDetailActivity.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/View/TeamDetailActivity.kt
@@ -1,5 +1,6 @@
 package com.example.cloudymous.footballclubcloud.View
 
+import android.database.sqlite.SQLiteConstraintException
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -8,17 +9,25 @@ import android.support.design.R.attr.colorPrimary
 import android.support.v4.content.ContextCompat
 import android.support.v4.widget.SwipeRefreshLayout
 import android.view.Gravity
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.ProgressBar
 import android.widget.TextView
 import com.example.cloudymous.footballclubcloud.Api.ApiRepository
+import com.example.cloudymous.footballclubcloud.Db.database
+import com.example.cloudymous.footballclubcloud.Model.Favorite
 import com.example.cloudymous.footballclubcloud.Model.Team
 import com.example.cloudymous.footballclubcloud.Presenter.TeamDetailPresenter
+import com.example.cloudymous.footballclubcloud.R.id.add_to_favorite
+import com.example.cloudymous.footballclubcloud.R.menu.detail_menu
 import com.example.cloudymous.footballclubcloud.Utils.invisible
 import com.example.cloudymous.footballclubcloud.Utils.visible
 import com.google.gson.Gson
 import org.jetbrains.anko.*
+import org.jetbrains.anko.db.insert
+import org.jetbrains.anko.design.snackbar
 import org.jetbrains.anko.support.v4.onRefresh
 import org.jetbrains.anko.support.v4.swipeRefreshLayout
 
@@ -35,6 +44,9 @@ class TeamDetailActivity : AppCompatActivity(), TeamDetailView {
     private lateinit var presenter: TeamDetailPresenter
     private lateinit var teams: Team
     private lateinit var id: String
+
+    private var menuItem : Menu? = null
+    private var isFavorite: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -124,6 +136,42 @@ class TeamDetailActivity : AppCompatActivity(), TeamDetailView {
         teamFormedYear.text = data[0].teamFormatYear
         teamStadium.text = data[0].teamStadium
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(detail_menu, menu)
+        menuItem = menu
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            add_to_favorite -> {
+                addToFavorite()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun addToFavorite(){
+        try {
+            database.use {
+                insert(Favorite.TABLE_FAVORITE,
+                    Favorite.TEAM_ID to teams.teamId,
+                    Favorite.TEAM_NAME to teams.teamName,
+                    Favorite.TEAM_BADGE to teams.teamBadge)
+            }
+            snackbar(swipeRefresh, "Added to Favorite!").show()
+        }
+        catch (e: SQLiteConstraintException) {
+            snackbar(swipeRefresh, e.localizedMessage).show()
+        }
     }
 
 }
