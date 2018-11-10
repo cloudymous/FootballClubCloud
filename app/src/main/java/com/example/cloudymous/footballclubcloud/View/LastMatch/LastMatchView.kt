3e112e5632c201251566b391302a6477bdd215da
cloudymous <sayacloud@gmail.com>
2018-11-10 22:30:35 +0800

    Clear unused import.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/View/LastMatch/LastMatchView.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/View/LastMatch/LastMatchView.kt
@@ -1,7 +1,6 @@
 package com.example.cloudymous.footballclubcloud.View.LastMatch
 
 import com.example.cloudymous.footballclubcloud.Model.LastMatch
-import com.example.cloudymous.footballclubcloud.Model.Team
 
 interface LastMatchView {
     fun showLoading()
