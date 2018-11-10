9a6d6f1cc5eaacd111703331666b1ac0620c55d4
cloudymous <sayacloud@gmail.com>
2018-11-10 21:42:22 +0800

    Create detail match presenter

--- app/src/main/java/com/example/cloudymous/footballclubcloud/Presenter/DetailMatchPresenter.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/Presenter/DetailMatchPresenter.kt
@@ -1,4 +1,30 @@
 package com.example.cloudymous.footballclubcloud.Presenter
 
-class DetailMatchPresenter {
+import com.example.cloudymous.footballclubcloud.Api.ApiRepository
+import com.example.cloudymous.footballclubcloud.Api.TheSportDBApi
+import com.example.cloudymous.footballclubcloud.Model.DetailMatchResponse
+import com.example.cloudymous.footballclubcloud.View.DetailMatch.DetailMatchView
+import com.google.gson.Gson
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+
+class DetailMatchPresenter (private val view: DetailMatchView,
+                            private val apiRepository: ApiRepository,
+                            private val gson: Gson) {
+
+    fun getMatchDetail(matchId: String?){
+        view.showLoading()
+        doAsync {
+            val data = gson.fromJson(apiRepository
+                .doRequest(TheSportDBApi.getMatchDetail(matchId)),
+                DetailMatchResponse::class.java
+            )
+
+            uiThread {
+                view.hideLoading()
+                view.showDetailMatch(data.events)
+            }
+        }
+
+    }
 }
