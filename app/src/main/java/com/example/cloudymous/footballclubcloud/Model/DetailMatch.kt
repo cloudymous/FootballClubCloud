379e271217f6eb0e006a6392216fda2cd6b9003e
cloudymous <sayacloud@gmail.com>
2018-11-10 22:19:14 +0800

    Create DetailMatch model.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/Model/DetailMatch.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/Model/DetailMatch.kt
@@ -1,5 +1,22 @@
 package com.example.cloudymous.footballclubcloud.Model
 
+import com.google.gson.annotations.SerializedName
+
 data class DetailMatch (
+
+    @SerializedName("dateEvent")
+    var eventDate : String? = null,
+
+    @SerializedName("strHomeTeam")
+    var homeTeam : String? = null,
+
+    @SerializedName("strAwayTeam")
+    var awayTeam : String? = null,
+
+    @SerializedName("intHomeScore")
+    var homeScore : String? = null,
+
+    @SerializedName("intAwayScore")
+    var awayScore : String? = null
 
 )
