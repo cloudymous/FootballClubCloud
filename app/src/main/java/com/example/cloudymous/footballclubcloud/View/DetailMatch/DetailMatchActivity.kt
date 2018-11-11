049922b6723e0e9991999a99add216e67040fabc
cloudymous <sayacloud@gmail.com>
2018-11-11 20:36:59 +0800

    Now I see how to throw object to next intent.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/View/DetailMatch/DetailMatchActivity.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/View/DetailMatch/DetailMatchActivity.kt
@@ -3,6 +3,7 @@ package com.example.cloudymous.footballclubcloud.View.DetailMatch
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.cloudymous.footballclubcloud.Model.DetailMatch
+import com.example.cloudymous.footballclubcloud.Model.LastMatch
 import com.example.cloudymous.footballclubcloud.R
 import kotlinx.android.synthetic.main.activity_detail_match.*
 
@@ -16,13 +17,13 @@ class DetailMatchActivity : AppCompatActivity(){
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val intent = intent
-        val event = intent.getSerializableExtra("event") as? DetailMatch
+        val event = intent.getSerializableExtra("event") as LastMatch
 
-        date.text = event?.eventDate
-        home_team.text = event?.homeTeam
-        away_team.text = event?.awayTeam
-        home_score.text = event?.homeScore
-        away_score.text = event?.awayScore
+        date.text = event.eventDate
+        home_team.text = event.homeTeam
+        away_team.text = event.awayTeam
+        home_score.text = event.homeScore
+        away_score.text = event.awayScore
 
     }
 
