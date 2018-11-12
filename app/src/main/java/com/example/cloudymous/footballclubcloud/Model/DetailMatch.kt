482c6c21ce7fae8ca8328cd448ba05200fcfbe92
cloudymous <sayacloud@gmail.com>
2018-11-12 11:12:04 +0800

    Add more object to data class.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/Model/DetailMatch.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/Model/DetailMatch.kt
@@ -4,6 +4,7 @@ import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
 data class DetailMatch (
+
     @SerializedName("idEvent")
     var eventId : String? = null,
 
@@ -20,6 +21,48 @@ data class DetailMatch (
     var homeScore : String? = null,
 
     @SerializedName("intAwayScore")
-    var awayScore : String? = null
+    var awayScore : String? = null,
+
+    @SerializedName("strHomeRedCards")
+    var homeRedCards : String? = null,
+
+    @SerializedName("strAwayRedCards")
+    var awayRedCards : String? = null,
+
+    @SerializedName("strHomeYellowCards")
+    var homeYellowCards : String? = null,
+
+    @SerializedName("strAwayYellowCards")
+    var awayYellowCards : String? = null,
+
+    @SerializedName("strHomeLineupGoalkeeper")
+    var homeGoalKeeper : String? = null,
+
+    @SerializedName("strAwayLineupGoalkeeper")
+    var awayGoalKeeper : String? = null,
+
+    @SerializedName("strHomeLineupDefense")
+    var homeLineDefense : String? = null,
+
+    @SerializedName("strAwayLineupDefense")
+    var awayLineDefense : String? = null,
+
+    @SerializedName("strHomeLineupMidfield")
+    var homeLineMidfield : String? = null,
+
+    @SerializedName("strAwayLineupMidfield")
+    var awayLineMidfield : String? = null,
+
+    @SerializedName("strHomeLineupForward")
+    var homeLineForward : String? = null,
+
+    @SerializedName("strAwayLineupForward")
+    var awayLineForward : String? = null,
+
+    @SerializedName("strHomeLineupSubstitutes")
+    var homeSubtitutes : String? = null,
+
+    @SerializedName("strAwayLineupSubstitutes")
+    var awaySubtitutes : String? = null
 
 ) : Serializable
