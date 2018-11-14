0e6fbd60713293cdd7601e0c7a8683c0df261f16
cloudymous <sayacloud@gmail.com>
2018-11-14 14:03:40 +0800

    Create Favorite Team Fragment.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/View/Fragment/FavoriteTeamFragment.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/View/Fragment/FavoriteTeamFragment.kt
@@ -1,5 +1,66 @@
 package com.example.cloudymous.footballclubcloud.View.Fragment
 
+import android.content.Context
+import android.os.Bundle
+import android.support.design.R.attr.colorAccent
 import android.support.v4.app.Fragment
+import android.support.v4.widget.SwipeRefreshLayout
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.cloudymous.footballclubcloud.Model.Favorite
+import com.example.cloudymous.footballclubcloud.Presenter.FavoriteTeamAdapter
+import com.example.cloudymous.footballclubcloud.View.TeamDetailActivity
+import org.jetbrains.anko.*
+import org.jetbrains.anko.recyclerview.v7.recyclerView
+import org.jetbrains.anko.support.v4.swipeRefreshLayout
 
-class FavoriteTeamFragment : Fragment()
+class FavoriteTeamFragment : Fragment(), AnkoComponent<Context> {
+
+    private var favorites: MutableList<Favorite> = mutableListOf()
+
+    private lateinit var adapter: FavoriteTeamAdapter
+
+    private lateinit var swipeRefresh: SwipeRefreshLayout
+    private lateinit var listEvent: RecyclerView
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        adapter = FavoriteTeamAdapter(favorites){
+            requireContext().startActivity<TeamDetailActivity>("id" to "${it.teamId}")
+        }
+
+        listEvent.adapter = adapter
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return createView(AnkoContext.create(requireContext()))
+    }
+
+    override fun createView(ui: AnkoContext<Context>): View = with(ui) {
+        linearLayout {
+            lparams(width = matchParent, height = wrapContent)
+            topPadding = dip(16)
+            leftPadding = dip(16)
+            rightPadding = dip(16)
+
+            swipeRefresh = swipeRefreshLayout {
+                setColorSchemeResources(
+                    colorAccent,
+                    android.R.color.holo_green_light,
+                    android.R.color.holo_orange_light,
+                    android.R.color.holo_red_light
+                )
+
+                listEvent = recyclerView {
+                    lparams(width = matchParent, height = wrapContent)
+                    layoutManager = LinearLayoutManager(requireContext())
+                }
+            }
+        }
+    }
+}
