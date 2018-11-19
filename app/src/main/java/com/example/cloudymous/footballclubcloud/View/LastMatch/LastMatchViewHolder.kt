2cd5b8b649d6cb36db0cd3a17ce7f98acbafd1a3
cloudymous <sayacloud@gmail.com>
2018-11-19 10:20:40 +0800

    I just want to make my code structure easier to read.

---
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/View/LastMatch/LastMatchViewHolder.kt
@@ -0,0 +1,28 @@
+package com.example.cloudymous.footballclubcloud.View.LastMatch
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import com.example.cloudymous.footballclubcloud.Model.DetailMatch
+import com.example.cloudymous.footballclubcloud.Utils.formatDate
+import com.example.cloudymous.footballclubcloud.Utils.formatTime
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.list_match.*
+import org.jetbrains.anko.sdk25.coroutines.onClick
+
+class LastMatchViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer {
+
+    fun bindItem(lastmatch: DetailMatch, clickListener: (DetailMatch) -> Unit) {
+
+        date_match.text = formatDate(lastmatch.eventDate)
+
+        time_match.text = formatTime(lastmatch.eventTime)
+
+        home.text = lastmatch.homeTeam
+        home_score.text = lastmatch.homeScore
+
+        away.text = lastmatch.awayTeam
+        away_score.text = lastmatch.awayScore
+
+        itemView.onClick { clickListener(lastmatch) }
+    }
+}
