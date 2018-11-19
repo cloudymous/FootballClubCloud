9e67df76d0464661c7c01d31286f6bc1d9715dff
cloudymous <sayacloud@gmail.com>
2018-11-19 09:58:09 +0800

    Add formatDate function to utils.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/Utils/Utils.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/Utils/Utils.kt
@@ -1,11 +1,19 @@
 package com.example.cloudymous.footballclubcloud.Utils
 
 import android.view.View
+import java.text.SimpleDateFormat
 
-fun View.visible(){
+fun View.visible() {
     visibility = View.VISIBLE
 }
 
-fun View.invisible(){
+fun View.invisible() {
     visibility = View.INVISIBLE
+}
+
+fun formatDate(strDate: String?): String {
+    val simpleDateFormat = SimpleDateFormat("dd/MM/yyyy")
+    val date = simpleDateFormat.parse(strDate)
+    val dateWithDay = SimpleDateFormat("EEEE, dd MMM yyy")
+    return dateWithDay.format(date)
 }
