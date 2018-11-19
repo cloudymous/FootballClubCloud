0bba2d68dcd3d8a76d7aaa2eea1b79f0605b6a5e
cloudymous <sayacloud@gmail.com>
2018-11-19 10:22:38 +0800

    Same with previous commit.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/View/NextMatch/NextMatchAdapter.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/View/NextMatch/NextMatchAdapter.kt
@@ -3,15 +3,9 @@ package com.example.cloudymous.footballclubcloud.View.NextMatch
 import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import com.example.cloudymous.footballclubcloud.Model.DetailMatch
 import com.example.cloudymous.footballclubcloud.R
-import com.example.cloudymous.footballclubcloud.Utils.formatDate
-import com.example.cloudymous.footballclubcloud.Utils.formatTime
-import kotlinx.android.extensions.LayoutContainer
-import kotlinx.android.synthetic.main.list_match.*
-import org.jetbrains.anko.sdk25.coroutines.onClick
 
 class NextMatchAdapter(
     private val context: Context,
@@ -30,16 +24,4 @@ class NextMatchAdapter(
         holder.bindItem(nextmatch[position], clickListener)
     }
 
-}
-
-class NextMatchViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer {
-
-    fun bindItem(nextmatch: DetailMatch, clickListener: (DetailMatch) -> Unit) {
-        home.text = nextmatch.homeTeam
-        away.text = nextmatch.awayTeam
-        date_match.text = formatDate(nextmatch.eventDate)
-        time_match.text = formatTime(nextmatch.eventTime)
-
-        itemView.onClick { clickListener(nextmatch) }
-    }
 }
