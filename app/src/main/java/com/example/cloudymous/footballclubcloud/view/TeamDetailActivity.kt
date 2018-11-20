6a3edf75949ea0eb5d757afbbf2c4b8bd31bf114
cloudymous <sayacloud@gmail.com>
2018-11-20 21:24:48 +0800

    Create menu add to favorite and store in SQLite.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/view/TeamDetailActivity.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/view/TeamDetailActivity.kt
@@ -1,18 +1,25 @@
 package com.example.cloudymous.footballclubcloud.view
 
+import android.database.sqlite.SQLiteConstraintException
 import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.app.AppCompatActivity
 import android.view.Gravity
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.ProgressBar
 import android.widget.TextView
 import com.example.cloudymous.footballclubcloud.R
 import com.example.cloudymous.footballclubcloud.R.color.colorPrimaryText
+import com.example.cloudymous.footballclubcloud.R.id.add_to_favorite
+import com.example.cloudymous.footballclubcloud.R.menu.detail_menu
 import com.example.cloudymous.footballclubcloud.api.ApiRepository
+import com.example.cloudymous.footballclubcloud.db.database
+import com.example.cloudymous.footballclubcloud.model.Favorite
 import com.example.cloudymous.footballclubcloud.model.Team
 import com.example.cloudymous.footballclubcloud.presenter.TeamDetailPresenter
 import com.example.cloudymous.footballclubcloud.utils.invisible
@@ -20,6 +27,8 @@ import com.example.cloudymous.footballclubcloud.utils.visible
 import com.google.gson.Gson
 import com.squareup.picasso.Picasso
 import org.jetbrains.anko.*
+import org.jetbrains.anko.db.insert
+import org.jetbrains.anko.design.snackbar
 import org.jetbrains.anko.support.v4.onRefresh
 import org.jetbrains.anko.support.v4.swipeRefreshLayout
 
@@ -37,6 +46,9 @@ class TeamDetailActivity : AppCompatActivity(), TeamDetailView {
     private lateinit var presenter: TeamDetailPresenter
     private lateinit var teams: Team
     private lateinit var id: String
+
+    private var menuItem: Menu? = null
+    private var isFavorite: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -117,6 +129,27 @@ class TeamDetailActivity : AppCompatActivity(), TeamDetailView {
 
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(detail_menu, menu)
+        menuItem = menu
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            add_to_favorite -> {
+                addToFavorite()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
     override fun showLoading() {
         progressBarTeam.visible()
     }
@@ -139,5 +172,21 @@ class TeamDetailActivity : AppCompatActivity(), TeamDetailView {
         teamFormedYear.text = data[0].teamFormatYear
         teamStadium.text = data[0].teamStadium
 
+    }
+
+    private fun addToFavorite() {
+        try {
+            database.use {
+                insert(
+                    Favorite.TABLE_FAVORITE,
+                    Favorite.TEAM_ID to teams.teamId,
+                    Favorite.TEAM_NAME to teams.teamName,
+                    Favorite.TEAM_BADGE to teams.teamBadge
+                )
+            }
+            swipeRefresh.snackbar("Added to favorite").show()
+        } catch (e: SQLiteConstraintException) {
+            swipeRefresh.snackbar(e.localizedMessage).show()
+        }
     }
 }
