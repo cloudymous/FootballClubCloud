b10c7072a6faadfc2ad53e3023b7d8e9955c9956
cloudymous <sayacloud@gmail.com>
2018-11-20 20:00:29 +0800

    Change view. Wrong view. So bad I just realized that now!

--- app/src/main/java/com/example/cloudymous/footballclubcloud/presenter/TeamPresenter.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/presenter/TeamPresenter.kt
@@ -3,14 +3,16 @@ package com.example.cloudymous.footballclubcloud.presenter
 import com.example.cloudymous.footballclubcloud.api.ApiRepository
 import com.example.cloudymous.footballclubcloud.api.TheSportDBApi
 import com.example.cloudymous.footballclubcloud.model.TeamResponse
-import com.example.cloudymous.footballclubcloud.view.TeamView
+import com.example.cloudymous.footballclubcloud.view.TeamDetailView
 import com.google.gson.Gson
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
-class TeamPresenter (private val view: TeamView,
-                     private val apiRepository: ApiRepository,
-                     private val gson: Gson){
+class TeamPresenter(
+    private val view: TeamDetailView,
+    private val apiRepository: ApiRepository,
+    private val gson: Gson
+) {
 
     fun getTeamList(league: String?){
         view.showLoading()
@@ -22,7 +24,7 @@ class TeamPresenter (private val view: TeamView,
 
             uiThread {
                 view.hideLoading()
-                view.showTeamList(data.teams)
+                view.showTeamDetail(data.teams)
             }
         }
     }
