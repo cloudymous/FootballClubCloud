291daafece343d167a6ee9ea914fcc70e0210838
cloudymous <sayacloud@gmail.com>
2018-11-21 10:00:46 +0800

    Add getLastMatch and getNextMatch api.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/api/TheSportDBApi.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/api/TheSportDBApi.kt
@@ -28,4 +28,28 @@ object TheSportDBApi {
             .build()
             .toString()
     }
+
+    fun getLastMatch(leagueId: String?): String {
+        return Uri.parse(BuildConfig.BASE_URL).buildUpon()
+            .appendPath("api")
+            .appendPath("v1")
+            .appendPath("json")
+            .appendPath(BuildConfig.TSDB_API_KEY)
+            .appendPath("eventspastleague.php")
+            .appendQueryParameter("id", leagueId)
+            .build()
+            .toString()
+    }
+
+    fun getNextMatch(leagueId: String?): String {
+        return Uri.parse(BuildConfig.BASE_URL).buildUpon()
+            .appendPath("api")
+            .appendPath("v1")
+            .appendPath("json")
+            .appendPath(BuildConfig.TSDB_API_KEY)
+            .appendPath("eventsnextleague.php")
+            .appendQueryParameter("id", leagueId)
+            .build()
+            .toString()
+    }
 }
