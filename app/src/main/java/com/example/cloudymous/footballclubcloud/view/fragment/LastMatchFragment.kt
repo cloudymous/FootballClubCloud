4764acd5f4d182e832c3bb27df9230fa2cc60563
cloudymous <sayacloud@gmail.com>
2018-11-21 09:11:53 +0800

    Create new fragment.

---
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/view/fragment/LastMatchFragment.kt
@@ -0,0 +1,6 @@
+package com.example.cloudymous.footballclubcloud.view.fragment
+
+import android.support.v4.app.Fragment
+
+class LastMatchFragment : Fragment() {
+}
---
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/view/fragment/NextMatchFragment.kt
@@ -0,0 +1,6 @@
+package com.example.cloudymous.footballclubcloud.view.fragment
+
+import android.support.v4.app.Fragment
+
+class NextMatchFragment : Fragment() {
+}
