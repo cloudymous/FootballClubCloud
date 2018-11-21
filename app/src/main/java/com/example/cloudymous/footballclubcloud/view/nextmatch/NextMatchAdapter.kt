81280385173c96ffd7e9c7a75dee0410e2df3fe9
cloudymous <sayacloud@gmail.com>
2018-11-21 11:30:19 +0800

    Create next match adapter.

---
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/view/nextmatch/NextMatchAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.cloudymous.footballclubcloud.view.nextmatch
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import com.example.cloudymous.footballclubcloud.model.DetailMatch
+
+class NextMatchAdapter(
+    private val context: Context,
+    private val nextmatch: List<DetailMatch>,
+    private val clickListener: (DetailMatch) -> Unit
+) :
+    RecyclerView.Adapter<NextMatchViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NextMatchViewHolder =
+        NextMatchViewHolder(LayoutInflater.from(context).inflate(R.layout.list_match, parent, false))
+
+    override fun getItemCount(): Int = nextmatch.size
+
+    override fun onBindViewHolder(holder: NextMatchViewHolder, position: Int) {
+        holder.bindItem(nextmatch[position], clickListener)
+    }
+
+}
