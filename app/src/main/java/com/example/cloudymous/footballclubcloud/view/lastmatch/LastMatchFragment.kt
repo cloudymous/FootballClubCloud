dede693f760927337a1e0f342aef7f2f759f8341
cloudymous <sayacloud@gmail.com>
2018-11-21 10:26:05 +0800

    Finish last match fragment.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/view/lastmatch/LastMatchFragment.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/view/lastmatch/LastMatchFragment.kt
@@ -2,17 +2,63 @@ package com.example.cloudymous.footballclubcloud.view.lastmatch
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.cloudymous.footballclubcloud.R
+import com.example.cloudymous.footballclubcloud.api.ApiRepository
 import com.example.cloudymous.footballclubcloud.model.DetailMatch
+import com.example.cloudymous.footballclubcloud.presenter.LastMatchPresenter
+import com.example.cloudymous.footballclubcloud.utils.invisible
+import com.example.cloudymous.footballclubcloud.utils.visible
+import com.google.gson.Gson
+import kotlinx.android.synthetic.main.fragment_last_match.*
+import org.jetbrains.anko.support.v4.onRefresh
 
-class LastMatchFragment : Fragment() {
+class LastMatchFragment : Fragment(), LastMatchView {
 
     private var lastmatch: MutableList<DetailMatch> = mutableListOf()
 
     private lateinit var adapter: LastMatchAdapter
     private lateinit var presenter: LastMatchPresenter
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_last_match, container, false)
+    }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        adapter = LastMatchAdapter(requireContext(), lastmatch) {
+        }
+
+        last_match_list.layoutManager = LinearLayoutManager(context)
+        last_match_list.adapter = adapter
+
+        val request = ApiRepository()
+        val gson = Gson()
+
+        val leagueId = resources.getString(R.string.leagueId)
+        presenter = LastMatchPresenter(this, request, gson)
+        presenter.getLastMatch(leagueId)
+        swipe_refresh.onRefresh {
+            presenter.getLastMatch(leagueId)
+        }
+
+    }
+
+    override fun showLoading() {
+        progress_bar.visible()
+    }
+    override fun hideLoading() {
+        progress_bar.invisible()
+    }
+    override fun showLastMatchList(data: List<DetailMatch>) {
+        swipe_refresh.isRefreshing = false
+        lastmatch.clear()
+        lastmatch.addAll(data)
+        adapter.notifyDataSetChanged()
     }
 
 }
