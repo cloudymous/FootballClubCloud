7928ab546f977011c1b7bdf7c3901b791cb2fc17
cloudymous <sayacloud@gmail.com>
2018-11-21 13:15:54 +0800

    Create favorite match model.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/model/FavoriteMatch.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/model/FavoriteMatch.kt
@@ -4,14 +4,16 @@ data class FavoriteMatch (val events : List<DetailMatch>
 ) {
 
     companion object {
-        const val TABLE_FAVORITE: String = "TABLE_FAVORITE"
+        const val TABLE_FAVORITE_MATCH: String = "TABLE_FAVORITE_MATCH"
         const val ID: String = "ID_"
         const val EVENT_ID: String = "EVENT_ID"
         const val EVENT_DATE: String = "EVENT_DATE"
         const val EVENT_TIME: String = "EVENT_TIME"
         const val EVENT_HOME_TEAM_ID: String = "EVENT_HOME_TEAM_ID"
+        const val EVENT_HOME_BADGE: String = "EVENT_HOME_BADGE"
         const val EVENT_HOME_TEAM: String = "EVENT_HOME_TEAM"
         const val EVENT_AWAY_TEAM_ID: String = "EVENT_AWAY_TEAM_ID"
+        const val EVENT_AWAY_BADGE: String = "EVENT_AWAY_BADGE"
         const val EVENT_AWAY_TEAM: String = "EVENT_AWAY_TEAM"
         const val EVENT_HOME_SCORE: String = "EVENT_HOME_SCORE"
         const val EVENT_AWAY_SCORE: String = "EVENT_AWAY_SCORE"
