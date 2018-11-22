ff9888b73049b6ceb98ac6ffbf082c6fbd7fc628
cloudymous <sayacloud@gmail.com>
2018-11-22 09:31:56 +0800

    Create Favorite Match View Holder.

---
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/view/favorite/FavoriteMatchViewHolder.kt
@@ -0,0 +1,29 @@
+package com.example.cloudymous.footballclubcloud.view.favorite
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import com.example.cloudymous.footballclubcloud.model.FavoriteMatch
+import com.example.cloudymous.footballclubcloud.utils.formatDate
+import com.example.cloudymous.footballclubcloud.utils.formatTime
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.list_match.*
+import org.jetbrains.anko.sdk25.coroutines.onClick
+
+class FavoriteMatchViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView),
+    LayoutContainer {
+
+    fun bindItem(favoriteMatch: FavoriteMatch, clickListener: (FavoriteMatch) -> Unit) {
+
+        date_match.text = formatDate(favoriteMatch.eventDate)
+
+        time_match.text = formatTime(favoriteMatch.eventTime)
+
+        home.text = favoriteMatch.homeTeam
+        home_score.text = favoriteMatch.homeScore
+
+        away.text = favoriteMatch.awayTeam
+        away_score.text = favoriteMatch.awayScore
+
+        itemView.onClick { clickListener(favoriteMatch) }
+    }
+}
