0e2e30bc34f5f1a7dc4f00ea01b21d66b7d12cb3
cloudymous <sayacloud@gmail.com>
2018-11-22 09:59:40 +0800

    Remove unused class-

--- app/src/main/java/com/example/cloudymous/footballclubcloud/view/favorite/FavoriteViewHolder.kt
+++
@@ -1,6 +0,0 @@
-package com.example.cloudymous.footballclubcloud.view.favorite
-
-import android.support.v7.widget.RecyclerView
-import android.view.View
-
-class FavoriteViewHolder (override val containerView: View) : RecyclerView.ViewHolder
