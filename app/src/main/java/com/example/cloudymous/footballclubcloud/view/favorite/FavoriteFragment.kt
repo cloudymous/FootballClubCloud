6b1a713a484d64462f0bd1a2fae2ea6c966cfb86
cloudymous <sayacloud@gmail.com>
2018-11-22 09:51:04 +0800

    Create function to show favorites from database.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/view/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/view/favorite/FavoriteFragment.kt
@@ -3,10 +3,14 @@ package com.example.cloudymous.footballclubcloud.view.favorite
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
+import com.example.cloudymous.footballclubcloud.db.databaseFavorite
 import com.example.cloudymous.footballclubcloud.model.FavoriteMatch
 import com.example.cloudymous.footballclubcloud.view.detailmatch.DetailMatchActivity
 import kotlinx.android.synthetic.main.fragment_favorite_match.*
+import org.jetbrains.anko.db.classParser
+import org.jetbrains.anko.db.select
 import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.support.v4.onRefresh
 
 class FavoriteFragment : Fragment() {
 
@@ -23,6 +27,26 @@ class FavoriteFragment : Fragment() {
 
         favorite_match_list.layoutManager = LinearLayoutManager(context)
         favorite_match_list.adapter = adapter
+
+        swipe_refresh.onRefresh {
+            showFavorites()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        showFavorites()
+    }
+
+    private fun showFavorites() {
+        favoritesMatch.clear()
+        context?.databaseFavorite?.use {
+            swipe_refresh.isRefreshing = false
+            val result = select(FavoriteMatch.TABLE_FAVORITE_MATCH)
+            val favorites = result.parseList(classParser<FavoriteMatch>())
+            favoritesMatch.addAll(favorites)
+            adapter.notifyDataSetChanged()
+        }
     }
 
 }
