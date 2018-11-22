fc74afde19ca9f4a5b67db841fdf7a6e669077f3
cloudymous <sayacloud@gmail.com>
2018-11-22 09:36:05 +0800

    Complete the Favorite Match Adapter.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/view/favorite/FavoriteMatchAdapter.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/view/favorite/FavoriteMatchAdapter.kt
@@ -1,10 +1,24 @@
 package com.example.cloudymous.footballclubcloud.view.favorite
 
 import android.content.Context
-import com.example.cloudymous.footballclubcloud.model.DetailMatch
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.example.cloudymous.footballclubcloud.R
+import com.example.cloudymous.footballclubcloud.model.FavoriteMatch
 
 class FavoriteMatchAdapter(
     private val context: Context,
-    private val favorites: List<DetailMatch>,
-    private val listener: (DetailMatch) -> Unit
-)
+    private val favorites: List<FavoriteMatch>,
+    private val listener: (FavoriteMatch) -> Unit
+) : RecyclerView.Adapter<FavoriteMatchViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, position: Int): FavoriteMatchViewHolder =
+        FavoriteMatchViewHolder(LayoutInflater.from(context).inflate(R.layout.list_match, parent, false))
+
+    override fun getItemCount(): Int = favorites.size
+
+    override fun onBindViewHolder(holder: FavoriteMatchViewHolder, position: Int) {
+        holder.bindItem(favorites[position], listener)
+    }
+}
