09f2e213341dcd1c878c3ebefc103328059727d8
cloudymous <sayacloud@gmail.com>
2018-11-23 21:06:57 +0800

    Add detail match view function to show badge.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/view/detailmatch/DetailMatchView.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/view/detailmatch/DetailMatchView.kt
@@ -1,10 +1,12 @@
 package com.example.cloudymous.footballclubcloud.view.detailmatch
 
 import com.example.cloudymous.footballclubcloud.model.DetailMatch
+import com.example.cloudymous.footballclubcloud.model.Team
 
 interface DetailMatchView {
 
     fun showLoading()
     fun hideLoading()
     fun showTeam(data: List<DetailMatch>)
+    fun showBadge(dataHome: List<Team>, dataAway: List<Team>)
 }
