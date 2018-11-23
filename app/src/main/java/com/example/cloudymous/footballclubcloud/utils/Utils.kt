fda66e29a140518549d12defd17b08aa54159a76
cloudymous <sayacloud@gmail.com>
2018-11-24 00:03:52 +0800

    Gone wrong.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/utils/Utils.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/utils/Utils.kt
@@ -15,7 +15,7 @@ fun View.invisible(){
 fun formatDate(strDate: String?): String {
     val simpleDateFormat = SimpleDateFormat("dd/MM/yyyy")
     val date = simpleDateFormat.parse(strDate)
-    val dateWithDay = SimpleDateFormat("EEEE, dd MMM yyy")
+    val dateWithDay = SimpleDateFormat("EEEE, dd MMM yyyy")
     return dateWithDay.format(date)
 }
 
