9b5da65c761fa3fe06aa95d47c2f2240e14f8835
cloudymous <sayacloud@gmail.com>
2018-11-23 20:10:34 +0800

    Change data class parser to Favorite Match

--- app/src/main/java/com/example/cloudymous/footballclubcloud/view/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/view/favorite/FavoriteFragment.kt
@@ -8,9 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.cloudymous.footballclubcloud.R
 import com.example.cloudymous.footballclubcloud.db.databaseFavorite
-import com.example.cloudymous.footballclubcloud.model.DetailMatch
-import com.example.cloudymous.footballclubcloud.utils.invisible
-import com.example.cloudymous.footballclubcloud.utils.visible
+import com.example.cloudymous.footballclubcloud.model.FavoriteMatch
 import com.example.cloudymous.footballclubcloud.view.detailmatch.DetailMatchActivity
 import kotlinx.android.synthetic.main.fragment_favorite_match.*
 import org.jetbrains.anko.db.classParser
@@ -18,9 +16,9 @@ import org.jetbrains.anko.db.select
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.support.v4.onRefresh
 
-class FavoriteFragment : Fragment(), FavoriteView {
+class FavoriteFragment : Fragment() {
 
-    private var favoritesMatch: MutableList<DetailMatch> = mutableListOf()
+    private var favoritesMatch: MutableList<FavoriteMatch> = mutableListOf()
 
     private lateinit var adapter: FavoriteMatchAdapter
 
@@ -43,14 +41,6 @@ class FavoriteFragment : Fragment(), FavoriteView {
         return inflater.inflate(R.layout.fragment_favorite_match, container, false)
     }
 
-    override fun showLoading() {
-        progress_bar.visible()
-    }
-
-    override fun hideLoading() {
-        progress_bar.invisible()
-    }
-
     override fun onResume() {
         super.onResume()
         showFavorites()
@@ -60,8 +50,8 @@ class FavoriteFragment : Fragment(), FavoriteView {
         favoritesMatch.clear()
         context?.databaseFavorite?.use {
             swipe_refresh.isRefreshing = false
-            val result = select(DetailMatch.TABLE_FAVORITE_MATCH)
-            val favorites = result.parseList(classParser<DetailMatch>())
+            val result = select(FavoriteMatch.TABLE_FAVORITE_MATCH)
+            val favorites = result.parseList(classParser<FavoriteMatch>())
             favoritesMatch.addAll(favorites)
             adapter.notifyDataSetChanged()
         }
