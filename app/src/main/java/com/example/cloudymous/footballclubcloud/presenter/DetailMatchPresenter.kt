a3b2ea5631d6f7b65efbd83b328fbe89eccb9a42
cloudymous <sayacloud@gmail.com>
2018-11-23 21:22:11 +0800

    Oh no. I supposed to be there.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/presenter/DetailMatchPresenter.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/presenter/DetailMatchPresenter.kt
@@ -1,9 +1,10 @@
-package com.example.cloudymous.footballclubcloud.view.detailmatch
+package com.example.cloudymous.footballclubcloud.presenter
 
 import com.example.cloudymous.footballclubcloud.api.ApiRepository
 import com.example.cloudymous.footballclubcloud.api.TheSportDBApi
 import com.example.cloudymous.footballclubcloud.model.DetailMatchResponse
 import com.example.cloudymous.footballclubcloud.model.TeamResponse
+import com.example.cloudymous.footballclubcloud.view.detailmatch.DetailMatchView
 import com.google.gson.Gson
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
