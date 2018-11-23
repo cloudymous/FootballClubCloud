64dee0e80e81cbb7ddd566a2224f4a65b8a2a8e6
cloudymous <sayacloud@gmail.com>
2018-11-23 20:11:56 +0800

    Change data class parser to Favorite Match

--- app/src/main/java/com/example/cloudymous/footballclubcloud/db/FavoriteDatabaseOpenHelper.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/db/FavoriteDatabaseOpenHelper.kt
@@ -2,11 +2,11 @@ package com.example.cloudymous.footballclubcloud.db
 
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
-import com.example.cloudymous.footballclubcloud.model.DetailMatch
+import com.example.cloudymous.footballclubcloud.model.FavoriteMatch
 import org.jetbrains.anko.db.*
 
 class FavoriteDatabaseOpenHelper(ctx: Context) :
-    ManagedSQLiteOpenHelper(ctx, "com.example.cloudymous.footballclubcloud.model.DetailMatch.db", null, 1) {
+    ManagedSQLiteOpenHelper(ctx, "com.example.cloudymous.footballclubcloud.model.FavoriteMatch.db", null, 1) {
     companion object {
         private var instance: MyDatabaseOpenHelper? = null
 
@@ -21,37 +21,37 @@ class FavoriteDatabaseOpenHelper(ctx: Context) :
 
     override fun onCreate(db: SQLiteDatabase) {
         db.createTable(
-            DetailMatch.TABLE_FAVORITE_MATCH,
+            FavoriteMatch.TABLE_FAVORITE_MATCH,
             true,
-            DetailMatch.ID to INTEGER + PRIMARY_KEY + AUTOINCREMENT,
-            DetailMatch.EVENT_ID to TEXT + UNIQUE,
-            DetailMatch.EVENT_DATE to TEXT,
-            DetailMatch.EVENT_TIME to TEXT,
-            DetailMatch.EVENT_HOME_TEAM_ID to TEXT,
-            DetailMatch.EVENT_HOME_TEAM to TEXT,
-            DetailMatch.EVENT_AWAY_TEAM_ID to TEXT,
-            DetailMatch.EVENT_AWAY_TEAM to TEXT,
-            DetailMatch.EVENT_HOME_SCORE to TEXT,
-            DetailMatch.EVENT_AWAY_SCORE to TEXT,
-            DetailMatch.EVENT_HOME_REDCARDS to TEXT,
-            DetailMatch.EVENT_AWAY_REDCARDS to TEXT,
-            DetailMatch.EVENT_HOME_YELLOWCARDS to TEXT,
-            DetailMatch.EVENT_AWAY_YELLOWCARDS to TEXT,
-            DetailMatch.EVENT_HOME_GOALKEEPER to TEXT,
-            DetailMatch.EVENT_AWAY_GOALKEEPER to TEXT,
-            DetailMatch.EVENT_HOME_LINEDEFENSE to TEXT,
-            DetailMatch.EVENT_AWAY_LINEDEFENSE to TEXT,
-            DetailMatch.EVENT_HOME_LINEMID to TEXT,
-            DetailMatch.EVENT_AWAY_LINEMID to TEXT,
-            DetailMatch.EVENT_HOME_LINEFORWARD to TEXT,
-            DetailMatch.EVENT_AWAY_LINEFORWARD to TEXT,
-            DetailMatch.EVENT_HOME_SUBTITUTES to TEXT,
-            DetailMatch.EVENT_AWAY_SUBTITUTES to TEXT
+            FavoriteMatch.ID to INTEGER + PRIMARY_KEY + AUTOINCREMENT,
+            FavoriteMatch.EVENT_ID to TEXT + UNIQUE,
+            FavoriteMatch.EVENT_DATE to TEXT,
+            FavoriteMatch.EVENT_TIME to TEXT,
+            FavoriteMatch.EVENT_HOME_TEAM_ID to TEXT,
+            FavoriteMatch.EVENT_HOME_TEAM to TEXT,
+            FavoriteMatch.EVENT_AWAY_TEAM_ID to TEXT,
+            FavoriteMatch.EVENT_AWAY_TEAM to TEXT,
+            FavoriteMatch.EVENT_HOME_SCORE to TEXT,
+            FavoriteMatch.EVENT_AWAY_SCORE to TEXT
+//            com.example.cloudymous.footballclubcloud.model.FavoriteMatch.EVENT_HOME_REDCARDS to TEXT,
+//            com.example.cloudymous.footballclubcloud.model.FavoriteMatch.EVENT_AWAY_REDCARDS to TEXT,
+//            com.example.cloudymous.footballclubcloud.model.FavoriteMatch.EVENT_HOME_YELLOWCARDS to TEXT,
+//            com.example.cloudymous.footballclubcloud.model.FavoriteMatch.EVENT_AWAY_YELLOWCARDS to TEXT,
+//            com.example.cloudymous.footballclubcloud.model.FavoriteMatch.EVENT_HOME_GOALKEEPER to TEXT,
+//            com.example.cloudymous.footballclubcloud.model.FavoriteMatch.EVENT_AWAY_GOALKEEPER to TEXT,
+//            com.example.cloudymous.footballclubcloud.model.FavoriteMatch.EVENT_HOME_LINEDEFENSE to TEXT,
+//            com.example.cloudymous.footballclubcloud.model.FavoriteMatch.EVENT_AWAY_LINEDEFENSE to TEXT,
+//            com.example.cloudymous.footballclubcloud.model.FavoriteMatch.EVENT_HOME_LINEMID to TEXT,
+//            com.example.cloudymous.footballclubcloud.model.FavoriteMatch.EVENT_AWAY_LINEMID to TEXT,
+//            com.example.cloudymous.footballclubcloud.model.FavoriteMatch.EVENT_HOME_LINEFORWARD to TEXT,
+//            com.example.cloudymous.footballclubcloud.model.FavoriteMatch.EVENT_AWAY_LINEFORWARD to TEXT,
+//            com.example.cloudymous.footballclubcloud.model.FavoriteMatch.EVENT_HOME_SUBTITUTES to TEXT,
+//            com.example.cloudymous.footballclubcloud.model.FavoriteMatch.EVENT_AWAY_SUBTITUTES to TEXT
         )
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
-        db.dropTable(DetailMatch.TABLE_FAVORITE_MATCH, true)
+        db.dropTable(FavoriteMatch.TABLE_FAVORITE_MATCH, true)
     }
 }
 
