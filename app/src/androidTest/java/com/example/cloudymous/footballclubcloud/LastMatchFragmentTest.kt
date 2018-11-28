ecdd880c0d233520765334ec2e7a54373342cdca
cloudymous <sayacloud@gmail.com>
2018-11-28 11:27:12 +0800

    Implement instrumental testing.
    Show recyclerview last match list
    Go to position 5 on RecyclerView
    Add to Favorite
    Check if "Add to favorite" showed
    Press back
    Check buttom navigation menu
    Go to Favorites

--- app/src/androidTest/java/com/example/cloudymous/footballclubcloud/LastMatchFragmentTest.kt
+++ app/src/androidTest/java/com/example/cloudymous/footballclubcloud/LastMatchFragmentTest.kt
@@ -1,15 +1,15 @@
 package com.example.cloudymous.footballclubcloud
 
 import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.Espresso.pressBack
 import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.contrib.RecyclerViewActions
-import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
-import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import android.support.v7.widget.RecyclerView
-import com.example.cloudymous.footballclubcloud.R.id.last_match_list
+import com.example.cloudymous.footballclubcloud.R.id.*
 import com.example.cloudymous.footballclubcloud.view.HomeActivity
 import org.junit.Rule
 import org.junit.Test
@@ -23,12 +23,30 @@ class LastMatchFragmentTest {
     val activityRule = ActivityTestRule(HomeActivity::class.java)
 
     @Test
-    fun testRecyclerViewBehaviour() {
+    fun testAppBehaviour() {
+
         onView(withId(last_match_list))
             .check(matches(isDisplayed()))
+
         onView(withId(last_match_list)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(5))
+
         onView(withId(last_match_list)).perform(
             RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(5, click())
         )
+
+        onView(withId(add_to_favorite))
+            .check(matches(isDisplayed()))
+
+        onView(withId(add_to_favorite)).perform(click())
+
+        onView(withText("Add to favorite"))
+            .check(matches(isDisplayed()))
+        pressBack()
+
+        onView(withId(bottom_navigation))
+            .check(matches(isDisplayed()))
+
+        onView(withId(favorites)).perform(click())
+
     }
 }
