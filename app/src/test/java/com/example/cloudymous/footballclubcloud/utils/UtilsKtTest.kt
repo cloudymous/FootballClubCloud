7c71350e1e618c98c8feef73896ef5e6e0e8a9b8
cloudymous <sayacloud@gmail.com>
2018-11-28 10:16:02 +0800

    Create UtilsTest.

---
+++ app/src/test/java/com/example/cloudymous/footballclubcloud/utils/UtilsKtTest.kt
@@ -0,0 +1,29 @@
+package com.example.cloudymous.footballclubcloud.utils
+
+import org.junit.Test
+
+import org.junit.Assert.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+class UtilsKtTest {
+
+    @Test
+    fun formatDate() {
+
+        val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd")
+        val date = simpleDateFormat.parse("2018-11-26")
+        val dateWithDay = SimpleDateFormat("EEEE, dd MMM yyyy")
+        assertEquals("Senin, 26 Nov 2018", dateWithDay.format(date))
+    }
+
+    @Test
+    fun formatTime() {
+
+        val simpleDateFormat = SimpleDateFormat("HH:mm")
+        simpleDateFormat.timeZone = TimeZone.getTimeZone("GMT")
+        val time = simpleDateFormat.parse("20:00:00+00:00")
+        assertEquals("20:00", simpleDateFormat.format(time))
+
+    }
+}
