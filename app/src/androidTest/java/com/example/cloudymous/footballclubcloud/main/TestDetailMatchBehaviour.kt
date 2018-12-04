4e91e072928ba19326640bf4f47e69db7ae8a299
cloudymous <sayacloud@gmail.com>
2018-12-04 11:46:26 +0800

    Add more scenario.

--- app/src/androidTest/java/com/example/cloudymous/footballclubcloud/main/TestDetailMatchBehaviour.kt
+++ app/src/androidTest/java/com/example/cloudymous/footballclubcloud/main/TestDetailMatchBehaviour.kt
@@ -1,7 +1,6 @@
 package com.example.cloudymous.footballclubcloud.main
 
 import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.Espresso.pressBack
 import android.support.test.espresso.IdlingRegistry
 import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.assertion.ViewAssertions.matches
@@ -55,7 +54,12 @@ class TestDetailMatchBehaviour {
         onView(withId(away_badge))
             .check(matches(isDisplayed()))
 
-        pressBack()
+        onView(withId(team_home))
+            .check(matches(isDisplayed()))
+
+        onView(withId(team_away))
+            .check(matches(isDisplayed()))
+
     }
 
 }
