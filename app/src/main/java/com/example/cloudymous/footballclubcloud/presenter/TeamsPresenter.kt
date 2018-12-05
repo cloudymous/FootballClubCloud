c2dde6f088f293a2128a4f33015e7b32e04cb712
cloudymous <sayacloud@gmail.com>
2018-12-05 13:25:07 +0800

    Create team presenter

--- app/src/main/java/com/example/cloudymous/footballclubcloud/presenter/TeamsPresenter.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/presenter/TeamsPresenter.kt
@@ -2,15 +2,15 @@ package com.example.cloudymous.footballclubcloud.presenter
 
 import com.example.cloudymous.footballclubcloud.api.ApiRepository
 import com.example.cloudymous.footballclubcloud.api.TheSportDBApi
-import com.example.cloudymous.footballclubcloud.model.DetailMatchResponse
-import com.example.cloudymous.footballclubcloud.view.details.DetailMatchView
+import com.example.cloudymous.footballclubcloud.model.TeamResponse
+import com.example.cloudymous.footballclubcloud.view.teams.TeamView
 import com.google.gson.Gson
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class TeamsPresenter(
-    private val view: DetailMatchView,
+    private val view: TeamView,
     private val apiRepository: ApiRepository,
     private val gson: Gson
 ) {
@@ -21,10 +21,10 @@ class TeamsPresenter(
             val data = gson.fromJson(
                 apiRepository
                     .doRequest(TheSportDBApi.getTeams(leagueId)).await(),
-                DetailMatchResponse::class.java
+                TeamResponse::class.java
             )
 
-            view.showTeam(data.events)
+            view.showTeamList(data.teams)
             view.hideLoading()
         }
     }
