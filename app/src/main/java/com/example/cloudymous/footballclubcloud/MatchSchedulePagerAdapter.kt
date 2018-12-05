b53d4eebfe84ac31aaeebcec922a291bfd7a677f
cloudymous <sayacloud@gmail.com>
2018-12-05 16:33:37 +0800

    Create Pager Adapter

---
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/MatchSchedulePagerAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.cloudymous.footballclubcloud
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import com.example.cloudymous.footballclubcloud.view.matches.lastmatch.LastMatchFragment
+import com.example.cloudymous.footballclubcloud.view.matches.nextmatch.NextMatchFragment
+
+class MatchSchedulePagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    private val pages = listOf(
+        LastMatchFragment(),
+        NextMatchFragment()
+    )
+
+    override fun getItem(position: Int): Fragment {
+        return pages[position]
+    }
+
+    override fun getCount(): Int {
+        return pages.size
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when (position) {
+            0 -> "Last Match"
+            else -> "Next Match"
+        }
+    }
+}
