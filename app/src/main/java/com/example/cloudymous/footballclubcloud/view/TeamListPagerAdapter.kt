72c19bc4a943ca49d13e9dfc402a6fd95ccf57c0
cloudymous <sayacloud@gmail.com>
2018-12-11 14:02:10 +0800

    Create a package of teams.

---
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/view/TeamListPagerAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.cloudymous.footballclubcloud.view
+
+class TeamListPagerAdapter {
+}
---
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/view/TeamsListFragment.kt
@@ -0,0 +1,126 @@
+package com.example.cloudymous.footballclubcloud.view
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.widget.SwipeRefreshLayout
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import com.example.cloudymous.footballclubcloud.R
+import com.example.cloudymous.footballclubcloud.api.ApiRepository
+import com.example.cloudymous.footballclubcloud.model.Team
+import com.example.cloudymous.footballclubcloud.presenter.TeamsPresenter
+import com.example.cloudymous.footballclubcloud.utils.invisible
+import com.example.cloudymous.footballclubcloud.utils.visible
+import com.example.cloudymous.footballclubcloud.view.teams.TeamView
+import com.example.cloudymous.footballclubcloud.view.teams.TeamsAdapter
+import com.google.gson.Gson
+import org.jetbrains.anko.*
+import org.jetbrains.anko.recyclerview.v7.recyclerView
+import org.jetbrains.anko.support.v4.onRefresh
+import org.jetbrains.anko.support.v4.swipeRefreshLayout
+import org.jetbrains.anko.support.v4.toast
+
+class TeamsListFragment : Fragment(), AnkoComponent<Context>, TeamView {
+
+    private var teams: MutableList<Team> = mutableListOf()
+    private lateinit var presenter: TeamsPresenter
+    private lateinit var adapter: TeamsAdapter
+    private lateinit var spinner: Spinner
+    private lateinit var listTeam: RecyclerView
+    private lateinit var progressBar: ProgressBar
+    private lateinit var swipeRefresh: SwipeRefreshLayout
+    private lateinit var leagueName: String
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
+        val spinnerItems = resources.getStringArray(R.array.league)
+        val spinnerAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, spinnerItems)
+        spinner.adapter = spinnerAdapter
+
+        adapter = TeamsAdapter(teams) {
+            //            context?.startActivity<TeamDetailActivity>("id" to "${it.teamId}")
+            toast("Hello").show()
+        }
+        listTeam.adapter = adapter
+
+        val request = ApiRepository()
+        val gson = Gson()
+        presenter = TeamsPresenter(this, request, gson)
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                leagueName = spinner.selectedItem.toString()
+                presenter.getTeams(leagueName)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {}
+        }
+
+        swipeRefresh.onRefresh {
+            presenter.getTeams(leagueName)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return createView(AnkoContext.create(requireContext()))
+    }
+
+    override fun createView(ui: AnkoContext<Context>): View = with(ui) {
+        linearLayout {
+            lparams(width = matchParent, height = wrapContent)
+            orientation = LinearLayout.VERTICAL
+            topPadding = dip(16)
+            leftPadding = dip(16)
+            rightPadding = dip(16)
+
+            spinner = spinner {
+                id = R.id.spinner
+            }
+            swipeRefresh = swipeRefreshLayout {
+                setColorSchemeResources(
+                    R.color.colorAccent,
+                    android.R.color.holo_green_light,
+                    android.R.color.holo_orange_light,
+                    android.R.color.holo_red_light
+                )
+
+                relativeLayout {
+                    lparams(width = matchParent, height = wrapContent)
+
+                    listTeam = recyclerView {
+                        id = R.id.list_team
+                        lparams(width = matchParent, height = wrapContent)
+                        layoutManager = LinearLayoutManager(ctx)
+                    }
+
+                    progressBar = progressBar {
+                    }.lparams {
+                        centerHorizontally()
+                    }
+                }
+            }
+        }
+    }
+
+    override fun showLoading() {
+        progressBar.visible()
+    }
+
+    override fun hideLoading() {
+        progressBar.invisible()
+    }
+
+    override fun showTeamList(data: List<Team>) {
+        swipeRefresh.isRefreshing = false
+        teams.clear()
+        teams.addAll(data)
+        adapter.notifyDataSetChanged()
+    }
+
+}
