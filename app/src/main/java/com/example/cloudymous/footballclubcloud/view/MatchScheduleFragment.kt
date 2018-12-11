643ec377fe2fbca5821d9ac8efdd2dc3e96061a5
cloudymous <sayacloud@gmail.com>
2018-12-11 10:26:16 +0800

    Create fragment in fragment

--- app/src/main/java/com/example/cloudymous/footballclubcloud/view/MatchScheduleFragment.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/view/MatchScheduleFragment.kt
@@ -1,7 +1,9 @@
 package com.example.cloudymous.footballclubcloud.view
 
 import android.os.Bundle
+import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
+import android.support.v4.view.ViewPager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,9 +12,22 @@ import com.example.cloudymous.footballclubcloud.R
 class MatchScheduleFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_match_schedule, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val pager = view.findViewById<ViewPager>(R.id.matches_pager_main)
+        val tabs = view.findViewById<TabLayout>(R.id.tabs_main)
+
+//        setHasOptionsMenu(true)
+
+        pager.adapter = MatchSchedulePagerAdapter(childFragmentManager)
+        tabs.setupWithViewPager(pager)
     }
 }
