4ec33b0ac6d5841fcc9a12c9180d53f6f2b9ae6b
cloudymous <sayacloud@gmail.com>
2018-12-11 11:49:23 +0800

    Rename some

--- app/src/main/java/com/example/cloudymous/footballclubcloud/view/matches/nextmatch/NextMatchFragment.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/view/matches/nextmatch/NextMatchFragment.kt
@@ -14,7 +14,7 @@ import com.example.cloudymous.footballclubcloud.utils.invisible
 import com.example.cloudymous.footballclubcloud.utils.visible
 import com.example.cloudymous.footballclubcloud.view.matches.detail.DetailMatchActivity
 import com.google.gson.Gson
-import kotlinx.android.synthetic.main.fragment_match_last.*
+import kotlinx.android.synthetic.main.fragment_match_next.*
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.support.v4.onRefresh
 
@@ -51,7 +51,7 @@ class NextMatchFragment : Fragment(), NextMatchView {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_match_last, container, false)
+        return inflater.inflate(R.layout.fragment_match_next, container, false)
     }
 
     override fun showLoading() {
