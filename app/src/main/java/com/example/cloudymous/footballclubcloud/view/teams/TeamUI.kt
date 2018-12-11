dbf468e7721fe482dc2a06b085833b5c5adbce8a
cloudymous <sayacloud@gmail.com>
2018-12-11 14:04:18 +0800

    Create Team UI and Holder

---
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/view/teams/TeamUI.kt
@@ -0,0 +1,35 @@
+package com.example.cloudymous.footballclubcloud.view.teams
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import com.example.cloudymous.footballclubcloud.R
+import org.jetbrains.anko.*
+
+class TeamUI : AnkoComponent<ViewGroup> {
+    override fun createView(ui: AnkoContext<ViewGroup>): View {
+        return with(ui) {
+            linearLayout {
+                lparams(width = matchParent, height = wrapContent)
+                padding = dip(16)
+                orientation = LinearLayout.HORIZONTAL
+
+                imageView {
+                    id = R.id.team_badge
+                }.lparams {
+                    height = dip(50)
+                    width = dip(50)
+                }
+
+                textView {
+                    id = R.id.team_name
+                    textSize = 16f
+                }.lparams {
+                    margin = dip(15)
+                }
+
+            }
+        }
+    }
+
+}
