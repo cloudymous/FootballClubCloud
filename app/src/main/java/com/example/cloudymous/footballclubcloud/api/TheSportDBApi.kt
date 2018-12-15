f70e41e4f3cce55b68a98a7c42d17e0cad0bbbc3
cloudymous <sayacloud@gmail.com>
2018-12-15 13:00:18 +0800

    Back to Uri parse.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/api/TheSportDBApi.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/api/TheSportDBApi.kt
@@ -1,71 +1,68 @@
 package com.example.cloudymous.footballclubcloud.api
 
+import android.net.Uri
+import com.example.cloudymous.footballclubcloud.BuildConfig
+
 object TheSportDBApi {
 
     fun getTeams(league: String?): String {
-//        return Uri.parse(BuildConfig.BASE_URL).buildUpon()
-//            .appendPath("api")
-//            .appendPath("v1")
-//            .appendPath("json")
-//            .appendPath(BuildConfig.TSDB_API_KEY)
-//            .appendPath("search_all_teams.php")
-//            .appendQueryParameter("l", league)
-//            .build()
-//            .toString()
-        return "https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=$league"
+        return Uri.parse(BuildConfig.BASE_URL).buildUpon()
+            .appendPath("api")
+            .appendPath("v1")
+            .appendPath("json")
+            .appendPath(BuildConfig.TSDB_API_KEY)
+            .appendPath("search_all_teams.php")
+            .appendQueryParameter("l", league)
+            .build()
+            .toString()
     }
 
     fun getTeamDetail(teamID: String?): String {
-//        return Uri.parse(BuildConfig.BASE_URL).buildUpon()
-//            .appendPath("api")
-//            .appendPath("v1")
-//            .appendPath("json")
-//            .appendPath(BuildConfig.TSDB_API_KEY)
-//            .appendPath("lookupteam.php")
-//            .appendQueryParameter("id", teamID)
-//            .build()
-//            .toString()
-        return "https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=$teamID"
+        return Uri.parse(BuildConfig.BASE_URL).buildUpon()
+            .appendPath("api")
+            .appendPath("v1")
+            .appendPath("json")
+            .appendPath(BuildConfig.TSDB_API_KEY)
+            .appendPath("lookupteam.php")
+            .appendQueryParameter("id", teamID)
+            .build()
+            .toString()
     }
 
     fun getLastMatch(leagueId: String?): String {
-//        return Uri.parse(BuildConfig.BASE_URL).buildUpon()
-//            .appendPath("api")
-//            .appendPath("v1")
-//            .appendPath("json")
-//            .appendPath(BuildConfig.TSDB_API_KEY)
-//            .appendPath("eventspastleague.php")
-//            .appendQueryParameter("id", leagueId)
-//            .build()
-//            .toString()
-        return "https://www.thesportsdb.com/api/v1/json/1/eventspastleague.php?id=$leagueId"
-
+        return Uri.parse(BuildConfig.BASE_URL).buildUpon()
+            .appendPath("api")
+            .appendPath("v1")
+            .appendPath("json")
+            .appendPath(BuildConfig.TSDB_API_KEY)
+            .appendPath("eventspastleague.php")
+            .appendQueryParameter("id", leagueId)
+            .build()
+            .toString()
     }
 
     fun getNextMatch(leagueId: String?): String {
-//        return Uri.parse(BuildConfig.BASE_URL).buildUpon()
-//            .appendPath("api")
-//            .appendPath("v1")
-//            .appendPath("json")
-//            .appendPath(BuildConfig.TSDB_API_KEY)
-//            .appendPath("eventsnextleague.php")
-//            .appendQueryParameter("id", leagueId)
-//            .build()
-//            .toString()
-        return "https://www.thesportsdb.com/api/v1/json/1/eventsnextleague.php?id=$leagueId"
+        return Uri.parse(BuildConfig.BASE_URL).buildUpon()
+            .appendPath("api")
+            .appendPath("v1")
+            .appendPath("json")
+            .appendPath(BuildConfig.TSDB_API_KEY)
+            .appendPath("eventsnextleague.php")
+            .appendQueryParameter("id", leagueId)
+            .build()
+            .toString()
     }
 
     fun getDetailMatch(eventId: String?): String {
-//        return Uri.parse(BuildConfig.BASE_URL).buildUpon()
-//            .appendPath("api")
-//            .appendPath("v1")
-//            .appendPath("json")
-//            .appendPath(BuildConfig.TSDB_API_KEY)
-//            .appendPath("lookupevent.php")
-//            .appendQueryParameter("id", eventId)
-//            .build()
-//            .toString()
-        return "https://www.thesportsdb.com/api/v1/json/1/lookupevent.php?id=$eventId"
+        return Uri.parse(BuildConfig.BASE_URL).buildUpon()
+            .appendPath("api")
+            .appendPath("v1")
+            .appendPath("json")
+            .appendPath(BuildConfig.TSDB_API_KEY)
+            .appendPath("lookupevent.php")
+            .appendQueryParameter("id", eventId)
+            .build()
+            .toString()
     }
 
 }
