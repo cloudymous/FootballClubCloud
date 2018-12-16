5a91d3a94dcc0d4c2c3cd94cfeda630b06676b08
cloudymous <sayacloud@gmail.com>
2018-12-16 17:11:33 +0800

    Create tabs and fragment for tabs team.

---
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/view/pageradapter/TeamListFragment.kt
@@ -0,0 +1,35 @@
+package com.example.cloudymous.footballclubcloud.view.pageradapter
+
+import android.os.Bundle
+import android.support.design.widget.TabLayout
+import android.support.v4.app.Fragment
+import android.support.v4.view.ViewPager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.cloudymous.footballclubcloud.R
+
+class TeamListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_match_schedule, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val pager = view.findViewById<ViewPager>(R.id.team_pager_main)
+        val tabs = view.findViewById<TabLayout>(R.id.tabs_main)
+
+//        setHasOptionsMenu(true)
+
+        pager.adapter = TeamListPagerAdapter(
+            childFragmentManager
+        )
+        tabs.setupWithViewPager(pager)
+    }
+}
---
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/view/pageradapter/TeamListPagerAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.cloudymous.footballclubcloud.view.pageradapter
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import com.example.cloudymous.footballclubcloud.view.teams.TeamsFragment
+
+class TeamListPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    private val pages = listOf(
+        TeamsFragment()
+    )
+
+    override fun getItem(position: Int): Fragment {
+        return pages[position]
+    }
+
+    override fun getCount(): Int {
+        return pages.size
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when (position) {
+            0 -> "Team List"
+            else -> "Favorite"
+        }
+    }
+}
