d570129b23aad3f9166e03172ede7abd580a8891
cloudymous <sayacloud@gmail.com>
2018-12-16 17:13:46 +0800

    Sampai sini dulu.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/view/pageradapter/TeamListPagerAdapter.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/view/pageradapter/TeamListPagerAdapter.kt
@@ -3,12 +3,14 @@ package com.example.cloudymous.footballclubcloud.view.pageradapter
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
+import com.example.cloudymous.footballclubcloud.view.matches.nextmatch.NextMatchFragment
 import com.example.cloudymous.footballclubcloud.view.teams.TeamsFragment
 
 class TeamListPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
     private val pages = listOf(
-        TeamsFragment()
+        TeamsFragment(),
+        NextMatchFragment()
     )
 
     override fun getItem(position: Int): Fragment {
