d014b95d2cbb9794aa6dbd1c2eab3e6e9c58cd7b
cloudymous <sayacloud@gmail.com>
2018-12-17 09:04:30 +0800

    Create Favorite Team Holder

--- app/src/main/java/com/example/cloudymous/footballclubcloud/view/favorites/teamsfavorite/FavoriteTeamViewHolder.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/view/favorites/teamsfavorite/FavoriteTeamViewHolder.kt
@@ -1,2 +1,18 @@
 package com.example.cloudymous.footballclubcloud.view.favorites.teamsfavorite
 
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import com.example.cloudymous.footballclubcloud.model.Team
+import com.squareup.picasso.Picasso
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.list_team.*
+
+class FavoriteTeamViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView),
+    LayoutContainer {
+
+    fun bindItem(teams: Team, listener: (Team) -> Unit) {
+        Picasso.get().load(teams.teamBadge).into(team_badge)
+        team_name.text = teams.teamName
+        itemView.setOnClickListener { listener(teams) }
+    }
+}
