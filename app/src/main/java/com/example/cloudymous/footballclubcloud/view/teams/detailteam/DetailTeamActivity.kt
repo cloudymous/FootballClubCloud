5e7d711091289a0d02d5022d9fcdec3cab00d581
cloudymous <sayacloud@gmail.com>
2018-12-17 10:00:34 +0800

    Import android R.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/view/teams/detailteam/DetailTeamActivity.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/view/teams/detailteam/DetailTeamActivity.kt
@@ -2,6 +2,7 @@ package com.example.cloudymous.footballclubcloud.view.teams.detailteam
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.example.cloudymous.footballclubcloud.R
 
 class DetailTeamActivity : AppCompatActivity() {
 
