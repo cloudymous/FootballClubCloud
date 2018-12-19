8b26b8cc24e9e58724afe48c224cb32c9ee43d7b
cloudymous <sayacloud@gmail.com>
2018-12-19 10:25:13 +0800

    Do I really need interface?

--- app/src/main/java/com/example/cloudymous/footballclubcloud/view/teams/detailteam/DetailTeamView.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/view/teams/detailteam/DetailTeamView.kt
@@ -3,7 +3,5 @@ package com.example.cloudymous.footballclubcloud.view.teams.detailteam
 import com.example.cloudymous.footballclubcloud.model.Team
 
 interface DetailTeamView {
-    fun showLoading()
-    fun hideLoading()
     fun showTeamDetail(data: List<Team>)
 }
