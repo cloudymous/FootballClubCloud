b8992270668c7e9e39d9893f74d8adf0a9522386
cloudymous <sayacloud@gmail.com>
2018-12-20 11:20:05 +0800

    Create Detail Team Presenter.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/presenter/DetailTeamPresenter.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/presenter/DetailTeamPresenter.kt
@@ -1,2 +1,30 @@
 package com.example.cloudymous.footballclubcloud.presenter
 
+import com.example.cloudymous.footballclubcloud.api.ApiRepository
+import com.example.cloudymous.footballclubcloud.api.TheSportDBApi
+import com.example.cloudymous.footballclubcloud.model.TeamResponse
+import com.example.cloudymous.footballclubcloud.view.teams.detailteam.DetailTeamView
+import com.google.gson.Gson
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+
+class DetailTeamPresenter(
+    private val view: DetailTeamView,
+    private val apiRepository: ApiRepository,
+    private val gson: Gson
+) {
+    fun getDetailTeam(teamId: String?) {
+        doAsync {
+            val data = gson.fromJson(
+                apiRepository
+                    .doRequest(TheSportDBApi.getTeamDetail(teamId)),
+                TeamResponse::class.java
+            )
+
+            uiThread {
+                view.showTeamDetail(data.teams)
+            }
+
+        }
+    }
+}
