b35cb2d6f6418ab20c51dd158086db7d14f37d12
cloudymous <sayacloud@gmail.com>
2018-12-20 12:28:23 +0800

    I dont know.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/view/teams/detailteam/DetailTeamOverviewFragment.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/view/teams/detailteam/DetailTeamOverviewFragment.kt
@@ -1,7 +1,40 @@
 package com.example.cloudymous.footballclubcloud.view.teams.detailteam
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.cloudymous.footballclubcloud.R
+import com.example.cloudymous.footballclubcloud.api.ApiRepository
+import com.example.cloudymous.footballclubcloud.model.Team
+import com.example.cloudymous.footballclubcloud.presenter.DetailTeamPresenter
+import com.google.gson.Gson
+import kotlinx.android.synthetic.main.fragment_team_overview.*
 
-class DetailTeamOverviewFragment : Fragment() {
+class DetailTeamOverviewFragment : Fragment(), DetailTeamView {
 
+    private lateinit var presenter: DetailTeamPresenter
+    private var id: String? = null
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        val intent = activity.intent
+        id = intent.getStringExtra("teamId")
+
+        val request = ApiRepository()
+        val gson = Gson()
+
+        presenter = DetailTeamPresenter(this, request, gson)
+        presenter.getDetailTeam(id)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_team_overview, container, false)
+    }
+
+    override fun showTeamDetail(data: List<Team>) {
+        team_overview.text = data[0].teamDescription
+    }
 }
