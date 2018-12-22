ac36b0ce5c42f46dc1583ad90827b00b9943f450
cloudymous <sayacloud@gmail.com>
2018-12-22 13:00:24 +0800

    Create Player Presenter.

---
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/presenter/PlayersPresenter.kt
@@ -0,0 +1,29 @@
+package com.example.cloudymous.footballclubcloud.presenter
+
+import com.example.cloudymous.footballclubcloud.api.ApiRepository
+import com.example.cloudymous.footballclubcloud.api.TheSportDBApi
+import com.example.cloudymous.footballclubcloud.model.PlayerResponse
+import com.example.cloudymous.footballclubcloud.view.teams.detailteam.DetailTeamPlayerView
+import com.google.gson.Gson
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+
+class PlayersPresenter(
+    private val view: DetailTeamPlayerView,
+    private val apiRepository: ApiRepository,
+    private val gson: Gson
+) {
+    fun getPlayers(playerId: String?) {
+        doAsync {
+            val data = gson.fromJson(
+                apiRepository
+                    .doRequest(TheSportDBApi.getTeamPlayer(playerId)),
+                PlayerResponse::class.java
+            )
+
+            uiThread {
+                view.showPlayerDetail(data.players)
+            }
+        }
+    }
+}
