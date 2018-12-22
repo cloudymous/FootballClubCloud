e4083f477d31782c25ae86be1625bf5a8d0ce044
cloudymous <sayacloud@gmail.com>
2018-12-22 14:09:35 +0800

    Add show loading and hide loading

--- app/src/main/java/com/example/cloudymous/footballclubcloud/view/teams/player/TeamPlayerView.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/view/teams/player/TeamPlayerView.kt
@@ -3,5 +3,7 @@ package com.example.cloudymous.footballclubcloud.view.teams.player
 import com.example.cloudymous.footballclubcloud.model.Player
 
 interface TeamPlayerView {
+    fun showLoading()
+    fun hideLoading()
     fun showPlayerDetail(data: List<Player>)
 }
