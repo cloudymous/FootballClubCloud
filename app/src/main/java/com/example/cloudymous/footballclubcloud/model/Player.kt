34a6e2fa0e4072a45a96165ae800d343b75b3791
cloudymous <sayacloud@gmail.com>
2018-12-22 12:33:45 +0800

    Create player model.

---
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/model/Player.kt
@@ -0,0 +1,30 @@
+package com.example.cloudymous.footballclubcloud.model
+
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+data class Player(
+    @SerializedName("idPlayer")
+    var playerId: String? = null,
+
+    @SerializedName("strThumb")
+    var playerThumb: String? = null,
+
+    @SerializedName("strCutout")
+    var playerImage: String? = null,
+
+    @SerializedName("strPlayer")
+    var playerName: String? = null,
+
+    @SerializedName("strPosition")
+    var playerPosition: String? = null,
+
+    @SerializedName("strHeight")
+    var playerHeight: String? = null,
+
+    @SerializedName("strWeight")
+    var playerWeight: String? = null,
+
+    @SerializedName("strDescriptionEN")
+    var playerDescription: String? = null
+) : Serializable
