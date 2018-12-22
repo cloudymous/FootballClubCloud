6733942a801b95c6abeecf96219a1298194cc5fc
cloudymous <sayacloud@gmail.com>
2018-12-22 13:00:48 +0800

    Create Detail Team Player View

---
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/view/teams/detailteam/DetailTeamPlayerView.kt
@@ -0,0 +1,7 @@
+package com.example.cloudymous.footballclubcloud.view.teams.detailteam
+
+import com.example.cloudymous.footballclubcloud.model.Player
+
+interface DetailTeamPlayerView {
+    fun showPlayerDetail(data: List<Player>)
+}
