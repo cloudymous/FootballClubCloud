a54c1a3f176876f80a5035661b072d56a96f83d6
cloudymous <sayacloud@gmail.com>
2018-12-23 13:27:50 +0800

    Sekarang hemat 2 file.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/ui/view/LastMatchView.kt
+++
@@ -1,9 +0,0 @@
-package com.example.cloudymous.footballclubcloud.ui.view
-
-import com.example.cloudymous.footballclubcloud.model.DetailMatch
-
-interface LastMatchView {
-    fun showLoading()
-    fun hideLoading()
-    fun showLastMatchList(data: List<DetailMatch>)
-}
