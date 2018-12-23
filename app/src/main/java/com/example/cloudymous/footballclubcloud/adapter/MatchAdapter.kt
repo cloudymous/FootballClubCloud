755cfe0e21b4f173dc0bbc0ce3bb8c3aed42b68d
cloudymous <sayacloud@gmail.com>
2018-12-23 11:44:59 +0800

    Create match adapter

---
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/adapter/MatchAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.cloudymous.footballclubcloud.adapter
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.example.cloudymous.footballclubcloud.R
+import com.example.cloudymous.footballclubcloud.adapter.holder.MatchViewHolder
+import com.example.cloudymous.footballclubcloud.model.DetailMatch
+
+class MatchAdapter(
+    private val context: Context,
+    private val match: List<DetailMatch>,
+    private val clickListener: (DetailMatch) -> Unit
+) :
+    RecyclerView.Adapter<MatchViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, position: Int): MatchViewHolder =
+        MatchViewHolder(
+            LayoutInflater.from(context).inflate(
+                R.layout.list_match,
+                parent,
+                false
+            )
+        )
+
+    override fun getItemCount(): Int = match.size
+
+    override fun onBindViewHolder(holder: MatchViewHolder, position: Int) {
+        holder.bindItem(match[position], clickListener)
+    }
+}
