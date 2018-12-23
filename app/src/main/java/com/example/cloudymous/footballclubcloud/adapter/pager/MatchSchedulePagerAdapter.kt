ad49fbe7db7c35b337f5fe3978f10eed79a0ab36
cloudymous <sayacloud@gmail.com>
2018-12-23 13:23:44 +0800

    Same with previous commit.

--- app/src/main/java/com/example/cloudymous/footballclubcloud/adapter/pager/MatchSchedulePagerAdapter.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/adapter/pager/MatchSchedulePagerAdapter.kt
@@ -3,30 +3,26 @@ package com.example.cloudymous.footballclubcloud.adapter.pager
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
-import com.example.cloudymous.footballclubcloud.ui.fragment.LastMatchFragment
-import com.example.cloudymous.footballclubcloud.ui.fragment.NextMatchFragment
-import com.example.cloudymous.footballclubcloud.ui.fragment.TeamsFragment
 
 class MatchSchedulePagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
-    private val pages = listOf(
-        LastMatchFragment(),
-        NextMatchFragment(),
-        TeamsFragment()
-    )
+    val fragmentPages = ArrayList<Fragment>()
+    val fragmentTitleList = ArrayList<String>()
 
     override fun getItem(position: Int): Fragment {
-        return pages[position]
+        return fragmentPages[position]
     }
 
     override fun getCount(): Int {
-        return pages.size
+        return fragmentPages.size
+    }
+
+    fun addFragment(fragment: Fragment, title: String) {
+        fragmentPages.add(fragment)
+        fragmentTitleList.add(title)
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
-        return when (position) {
-            0 -> "Last Match"
-            else -> "Next Match"
-        }
+        return fragmentTitleList[position]
     }
 }
