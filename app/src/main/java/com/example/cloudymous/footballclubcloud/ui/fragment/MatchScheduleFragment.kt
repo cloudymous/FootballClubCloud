2a20c1728e8801b01c6a041b7d32cecf4cfbffbb
cloudymous <sayacloud@gmail.com>
2018-12-23 17:41:47 +0800

    Complete seacrh funtion

--- app/src/main/java/com/example/cloudymous/footballclubcloud/ui/fragment/MatchScheduleFragment.kt
+++ app/src/main/java/com/example/cloudymous/footballclubcloud/ui/fragment/MatchScheduleFragment.kt
@@ -1,16 +1,55 @@
 package com.example.cloudymous.footballclubcloud.ui.fragment
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
 import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
 import android.support.v4.view.ViewPager
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.SearchView
+import android.view.*
 import com.example.cloudymous.footballclubcloud.R
+import com.example.cloudymous.footballclubcloud.adapter.MatchAdapter
 import com.example.cloudymous.footballclubcloud.adapter.pager.MatchSchedulePagerAdapter
+import com.example.cloudymous.footballclubcloud.api.ApiRepository
+import com.example.cloudymous.footballclubcloud.model.DetailMatch
+import com.example.cloudymous.footballclubcloud.presenter.MatchPresenter
+import com.example.cloudymous.footballclubcloud.ui.activity.DetailMatchActivity
+import com.example.cloudymous.footballclubcloud.ui.view.MatchView
+import com.example.cloudymous.footballclubcloud.utils.invisible
+import com.example.cloudymous.footballclubcloud.utils.visible
+import com.google.gson.Gson
+import org.jetbrains.anko.startActivity
 
-class MatchScheduleFragment : Fragment() {
+class MatchScheduleFragment : Fragment(), MatchView {
+
+    private var searchMatch: MutableList<DetailMatch> = mutableListOf()
+    private var searchView: SearchView? = null
+
+    private lateinit var queryTextListener: SearchView.OnQueryTextListener
+    private lateinit var rcSearchView: RecyclerView
+    private lateinit var adapter: MatchAdapter
+    private lateinit var presenter: MatchPresenter
+    private lateinit var pager: ViewPager
+    private lateinit var tabs: TabLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        val request = ApiRepository()
+        val gson = Gson()
+        presenter = MatchPresenter(this, request, gson)
+        rcSearchView.invisible()
+        pager.visible()
+        tabs.visible()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,15 +62,84 @@ class MatchScheduleFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val pager = view.findViewById<ViewPager>(R.id.matches_pager_main)
-        val tabs = view.findViewById<TabLayout>(R.id.tabs_main)
-
-//        setHasOptionsMenu(true)
+        pager = view.findViewById(R.id.matches_pager_main)
+        tabs = view.findViewById(R.id.tabs_main)
+        rcSearchView = view.findViewById(R.id.search_result)
 
         val adapter = MatchSchedulePagerAdapter(childFragmentManager)
         adapter.addFragment(LastMatchFragment(), "Last Match")
         adapter.addFragment(NextMatchFragment(), "Next Match")
         pager.adapter = adapter
         tabs.setupWithViewPager(pager)
+        rcSearchView.layoutManager = LinearLayoutManager(activity)
     }
+
+
+    override fun hideLoading() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showLoading() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showMatchList(data: List<DetailMatch>) {
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.search_menu, menu)
+        val searchItem: MenuItem = menu.findItem(R.id.search_action)
+        val searchManager: SearchManager = activity?.getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        if (searchItem != null) {
+            searchView = searchItem.actionView as SearchView
+        }
+        if (searchItem !== null) {
+            searchView?.setSearchableInfo(searchManager.getSearchableInfo(activity?.componentName))
+            queryTextListener = object : SearchView.OnQueryTextListener {
+                override fun onQueryTextChange(newText: String): Boolean {
+                    return true
+                }
+
+                override fun onQueryTextSubmit(query: String): Boolean {
+                    var querySearch = query
+                    querySearch.toLowerCase()
+                    querySearch = querySearch.replace(" ", "_")
+                    adapter = MatchAdapter(requireContext(), searchMatch) {
+                        requireContext().startActivity<DetailMatchActivity>("eventId" to "${it.eventId}")
+                    }
+                    rcSearchView.adapter = adapter
+                    presenter.getSearchResult(querySearch)
+                    return true
+                }
+            }
+            searchView?.setOnQueryTextListener(queryTextListener)
+        }
+
+        searchItem.setOnActionExpandListener(object : MenuItem.OnActionExpandListener {
+            override fun onMenuItemActionExpand(menuItem: MenuItem): Boolean {
+                return true
+            }
+
+            override fun onMenuItemActionCollapse(item: MenuItem?): Boolean {
+                rcSearchView.invisible()
+                pager.visible()
+                tabs.visible()
+                return true
+            }
+        })
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.search_action -> {
+                return false
+            }
+        }
+        searchView?.setOnQueryTextListener(queryTextListener)
+        return super.onOptionsItemSelected(item)
+    }
+
 }
