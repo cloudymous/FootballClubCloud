1bbfa573d07a814d0fb69e725e261dac2be560c6
cloudymous <sayacloud@gmail.com>
2018-12-23 09:13:47 +0800

    Delete unused

--- app/src/main/java/com/example/cloudymous/footballclubcloud/view/teams/detailplayer/DetailPlayerView.kt
+++
@@ -1,8 +0,0 @@
-package com.example.cloudymous.footballclubcloud.view.teams.detailplayer
-
-import com.example.cloudymous.footballclubcloud.model.Player
-
-interface DetailPlayerView {
-
-    fun showPlayerDetail(data: List<Player>)
-}
